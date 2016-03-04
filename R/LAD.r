fa5e048d5ba475bca5140799bad2e3ff9f96fcf4
Jean-Romain <jromain.roussel@gmail.com>
2016-03-04 18:08:54 -0500

    Fixed : bug with infinite values in LAD and add the k parameter in LAD

--- R/LAD.r
+++ R/LAD.r
@@ -4,8 +4,8 @@
 #'
 #' @param z vector of positive z coordinates
 #' @param by numeric. The thickeness of the layers used (height bin)
-#' @param threshold numeric. Remove points below the threshold
-#' @return numeric vector containing the leaf area density for each bin
+#' @param k numeric. is the extinction coefficient
+#' @return A data.frame containing the bins elevations (z) and leaf area density for each bin (lad)
 #' @examples
 #' z = dbeta(seq(0, 0.8, length = 1000), 6, 6)*10
 #'
@@ -14,25 +14,42 @@
 #' plot(1:length(lad)~lad, type="l", ylab="Elevation", xlab="Leaf area density")
 #' @references Bouvier, M., Durrieu, S., Fournier, R. a, & Renaud, J. (2015).  Generalizing predictive models of forest inventory attributes using an area-based approach with airborne LiDAR data. Remote Sensing of Environment, 156, 322-334. http://doi.org/10.1016/j.rse.2014.10.004
 #' @export LAD
-LAD = function(z, by=1, threshold = 0) # (Bouvier et al. 2015)
+LAD = function(z, by = 1, k = 0.5) # (Bouvier et al. 2015)
 {
-	maxz = max(z)
-	z = z[z > threshold]
+	ld = .leafdensity(z, by)
 
-	if(maxz < 3)
-		return(NA_real_)
+	lad = 1-ld$ld
+	lad = -log(lad)/k
 
-	bk = seq(0, ceiling(maxz), 1)
-	histogram = hist(z, breaks = bk, plot=F)$count
+	lad[is.infinite(lad)] = NA
 
-	cs = cumsum(histogram)
-	lad = cs/dplyr::lead(cs)
+	z = ld$z[-1]
+	lad = lad[-1]
 
-	lad[is.nan(lad)] = 0
-	lad[is.infinite(lad)] = 1
-
-	lad = lad[1:(length(lad)-1)]
-	lad = -2*log(lad)
-
-	return(lad)
+	return(data.frame(z = ld$z, lad))
 }
+
+.leafdensity = function (z, by = 1)
+{
+    maxz = max(z)
+
+    bk = seq(0, ceiling(maxz), by)
+
+    histogram = hist(z, breaks = bk, plot = F)
+    height    = histogram$mids
+    histogram = histogram$counts
+
+    cs = cumsum(histogram)
+    r = (dplyr::lead(cs)-cs)
+    r[is.na(r)] = 0
+    r = r[-length(r)]
+
+    r = c(length(z)-sum(r), r)
+
+    r = r/cumsum(r)
+
+    r[is.nan(r)] = NA
+
+    return(data.frame(z = height, ld = r))
+}
+
