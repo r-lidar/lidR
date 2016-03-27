cf36dfe3177526b543a7716bcad7391c2f87785a
Jean-Romain <jromain.roussel@gmail.com>
2016-03-26 20:25:53 -0400

    Improve error messages

--- R/testFuncSignature.r
+++ R/testFuncSignature.r
@@ -8,15 +8,34 @@
 {
   funcstring = deparse(func)
 
+  begin_err = paste("The expression '", funcstring, "' returned a", sep = "")
+  advice = "A single number or a list of single number is expected."
+
   if(is.list(metrics) & !is.data.frame(metrics))
   {
     classes = sapply(metrics, class)
-    test = classes %in% c("integer", "numeric", "logical")
+    test = classes %in% c("integer", "numeric", "logical", "character")
     n = names(metrics[!test])
     c = classes[!test]
 
-    if(sum(!test) > 0)
-      stop(paste("The function '", funcstring, "' returned a list in which all elements are not a single numeric or logical value. The elements: '", capture.output(cat(n)), "' are respectively of classes: '", capture.output(cat(c)), "'", sep=""), call.=F)
+    if(sum(!test) == 1)
+    {
+      stop(paste(begin_err,
+                 "list in which all elements are not a single numeric or logical value. The metric",
+                 n, "is a", c,
+                 sep=" "),
+           call.=F)
+    }
+    else if(sum(!test) > 1)
+    {
+      stop(paste(begin_err,
+                 "list in which all elements are not a single numeric or logical value. The metrics",
+                 capture.output(cat(n, sep=" and ")),
+                 "are respectively",
+                 capture.output(cat(c, sep= " and ")),
+                 sep=" "),
+           call.=F)
+    }
 
     size = sapply(metrics, length)
     test = size == 1
@@ -24,19 +43,29 @@
     n = names(metrics[!test])
     c = size[!test]
 
-    if(sum(!test) > 0)
-      stop(paste("The function '", funcstring, "' returned a list in which all elements are not a single value. The elements: '", capture.output(cat(n)), "' have respectively a length of: '", capture.output(cat(c)), "'", sep=""), call.=F)
+    if(sum(!test) == 1)
+    {
+      stop(paste(begin_err,
+                 "list in which all elements are not a single value. The metric",
+                 n, "has a length of", c,
+                 sep=" "),
+           call.=F)
+    }
+    else if(sum(!test) > 1)
+    {
+      stop(paste(begin_err,
+                 "list in which all elements are not a single value. The metrics:",
+                 capture.output(cat(n, sep=" and ")),
+                 "have respectively a length of:",
+                 capture.output(cat(c, sep=" and ")),
+                 sep=" "),
+           call.=F)
+    }
   }
   else if(is.data.frame(metrics))
-  {
-    stop(paste("The function '", funcstring, "' returned a data.frame. A single number or a list of single number is expected.", sep=""), call.=F)
-  }
-  else if(is.numeric(metrics))
-  {
-    if(is.vector(metrics) & length(metrics) > 1)
-      stop(paste("The function '", funcstring, "' returned a vector. A single number or a list of single number is expected.", sep=""), call.=F)
-
-  }
+    stop(paste(begin_err,  "a data.frame", advice, sep=" "), call.=F)
+  else if(is.vector(metrics) & length(metrics) > 1)
+    stop(paste(begin_err, "a vector of lenght ", length(metrics), advice, sep=" "), call.=F)
   else
     return(0)
 }
