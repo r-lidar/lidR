51215cdc13831ebb4ffafce06b0607951bf65a07
jr <jromain.roussel@gmail.com>
2016-07-25 21:23:14 -0400

    Add filename in plot
    Former-commit-id: 4536fbf31214d6b10441bde2ffe3b01a3aa2d646
    Former-commit-id: 6d0fada162415cc147b3b57830265bb40be77a70
    Former-commit-id: 2e85ceb47fdd123985296d6af31e5beb1d39f818
    Former-commit-id: 2c93e0d97f09dd6ce7c127ae6255cdf1ec6a803e
    Former-commit-id: 6700f0e96ab214890dac240d323e4c2a89553cca

--- R/plot.catalog.r
+++ R/plot.catalog.r
@@ -6,7 +6,8 @@
 #' @param y Unused (inherited from base plot)
 #' @param \dots Unused (inherited from base plot)
 #' @export
-#' @importFrom graphics plot rect
+#' @importFrom graphics plot rect text
+#' @importFrom magrittr %$%
 plot.Catalog = function(x, y, ...)
 {
   headers = x@headers
@@ -17,5 +18,6 @@ plot.Catalog = function(x, y, ...)
   ymax = max(headers$Max.Y)
 
   graphics::plot(0,0, xlim=c(xmin, xmax), ylim = c(ymin, ymax), col="white", asp=1, xlab="X", ylab="Y")
-  graphics::rect(headers$Min.X, headers$Min.Y, headers$Max.X, headers$Max.Y)
+  headers %$% graphics::rect(Min.X, Min.Y, Max.X, Max.Y)
+  headers %$% graphics::text((Min.X + Max.X)/2, (Min.Y + Max.Y)/2, basename(filename))
 }
