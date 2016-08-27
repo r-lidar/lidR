19d1d0ece3287a4623e7013b8d38e10f048de4fc
jr <jromain.roussel@gmail.com>
2016-08-27 12:22:41 -0400

    Move indenfity* functions in there own file
    Former-commit-id: e0a2c463399c47edbf7ac9a59840d81576c305a1
    Former-commit-id: 0dff68bcaed24ab7aa092e521c1b3d9d2715f93c
    Former-commit-id: 57ddf223e4338404b248bff86a7be35eb136abcd
    Former-commit-id: f46289b2ad6628b263ac019de2ffc59d5f70ceb4
    Former-commit-id: ead6a393785243ad3766db6205a260cc32d3dc89

---
+++ R/indentify.r
@@ -0,0 +1,15 @@
+#' @importFrom dplyr lag
+.identify_pulse = function(return.number)
+{
+  boo = dplyr::lag(return.number) >= return.number
+  boo[1] = TRUE
+  return(cumsum(boo))
+}
+
+#' @importFrom dplyr lag
+.identify_flightlines = function(time, t = 30)
+{
+  boo = time - dplyr::lag(time) > t
+  boo[1] = TRUE
+  return(cumsum(boo))
+}
