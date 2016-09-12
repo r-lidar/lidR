24a84c490bdaa96468979096ffb9c69ee3ab40a8
JR <jean-romain.roussel.1@ulaval.ca>
2016-09-12 15:59:49 -0400

    Add indentify_scanline function

--- R/indentify.r
+++ R/indentify.r
@@ -9,7 +9,15 @@
 #' @importFrom dplyr lag
 .identify_flightlines = function(time, t = 30)
 {
-  boo = time - dplyr::lag(time) > t
+  boo = (time - dplyr::lag(time)) > t
+  boo[1] = TRUE
+  return(cumsum(boo))
+}
+
+#' @importFrom dplyr lag
+.identify_scanline = function(ScanDirectionFlag)
+{
+  boo = ScanDirectionFlag != dplyr::lag(ScanDirectionFlag)
   boo[1] = TRUE
   return(cumsum(boo))
 }
