b33c4bb3a2b30cb761254fa09bb8f27d44b64eaf
JR <jean-romain.roussel.1@ulaval.ca>
2016-11-28 15:32:43 -0500

    plot3d plot now a wireframe for RasterLayer object

---
+++ R/plot3d.r
@@ -0,0 +1,50 @@
+# ===============================================================================
+#
+# PROGRAMMERS:
+#
+# jean-romain.roussel.1@ulaval.ca  -  https://github.com/Jean-Romain/lidR
+#
+# COPYRIGHT:
+#
+# Copyright 2016 Jean-Romain Roussel
+#
+# This file is part of lidR R package.
+#
+# lidR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# ===============================================================================
+
+
+#' Plot a wireframe of a RasterLayer object
+#'
+#' @param x An object of the class RasterLayer
+#' @param y Unused (inherited from R base)
+#' @param add logical. if TRUE, add to current 3D plot.
+#' @param bg The color for the background. Default is black.
+#' @param \dots Supplementary parameters for \link[rgl:surface3d]{surface3d}
+#' @export
+#' @importFrom rgl surface3d open3d rgl.bg
+plot3d = function(x, y, add = FALSE, bg = "black", ...)
+{
+  inargs <- list(...)
+
+  mx = raster::as.matrix(x)
+  coord = sp::coordinates(x)
+
+  if(!add) rgl::open3d()
+
+  rgl::rgl.bg(color = bg)
+
+  rgl::surface3d(coord[,1], coord[,2], mx, front="lines", col="white", ...)
+}
