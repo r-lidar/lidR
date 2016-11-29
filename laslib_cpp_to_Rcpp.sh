c65e45a3a62bffdcea3d0bf1ada02fe82aaf017b
JR <jean-romain.roussel.1@ulaval.ca>
2016-11-29 15:32:48 -0500

    Document the code

--- laslib_cpp_to_Rcpp.sh
+++ laslib_cpp_to_Rcpp.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# The LASlib and LASzip are open source librairies developped by Martin Isenburg to read and write
+# las and laz file. I integrated these libraries into lidR to create a wrapper into R to deal
+# with las file.
+
+# However R does not like the way LASLib and LASzip are coded. R does not like rand and srand.
+# The use of exit(1), and the use of standard output (sdtout, stderr) are not allowed.
+
+# The following regex enable to automatically correct these problems:
+# - throw 1 replace exit(1);
+# - Rcpp::Rcerr << ... <<< std::end; replace fprintf(strerr, ...);
+# - Rcpp.h replace useless headers
+# - every words "stdout" even those in commented code (R is very diligent) were replaced by stdoutput
+# - srand have been commented
+# - R::runif replace rand
+# It represents at least 1000 lines automatically modified in 100 files this is why I can't list
+# all the modifications
+
+# Some extra changes were done
+# lasreaderpipeon.cpp 		  l96 		stdout > Rcpp::Rcout
+# laswriter.cpp			  l132-197 	comment lines
+# laswriter_las.cpp		  l96-105	comment lines
+# laswriter_qfit.cpp		  l82-91	comment lines
+# laswriter_bin.cpp		  l126-135	comment lines
+# bytestreamount_file.hpp 	  l134		stdout > Rcpp::Rcout
+
+# R is happy now!
+
+
 files=`find ./ -regex ".*[hc]pp"`
 
 ## ====== Remove every stderr, replace by Rcpp::cerr
@@ -35,28 +63,21 @@ perl -pi -w -e 's/fprintf\(stderr,\s*(\".*)(\%g)\s(\%g)\s(\%g)\svs\s(\%g)\s(\%g)
 perl -pi -w -e 's/fprintf\(stderr,\s*(\".*)(\%g)\s(\%g)\s(\%g)\s\/\s(\%g)\s(\%g)\s(\%g)(.*)(\\n\"),\s*([^,]*),\s*([^,]*),\s*([^,]*),\s*([^,]*),\s*([^,]*),\s*([^,]*)\);/Rcpp::Rcerr << $1" << $10 << " " << $11 << " " << $12 << " " << $13 << " " << $14 << " " << $15 << "$8" << std::endl;/g;' $files
 
 # Remove string and stdlib replaced by Rcpp
-perl -pi -w -e 's/#include <string.h>/#include <Rcpp.h>/g;' $files 
+perl -pi -w -e 's/#include <string.h>/#include <Rcpp.h>/g;' $files
 perl -pi -w -e 's/#include <stdio.h>/#include <stdio.h>\n#include <Rcpp.h>/g;' $files
 perl -pi -w -e 's/#include <stdlib.h>//g;' $files
 
 ## ====== Remove every exit(1), replace by throw 1
 
-perl -pi -w -e 's/exit\(1\)/throw 1/g;' $files 
+perl -pi -w -e 's/exit\(1\)/throw 1/g;' $files
 
 ## ====== Remove every stdout, replace by Rcpp::Rcout
 
-perl -pi -w -e 's/fileno\(stdout/fileno\(Rcpp::Rcout/g;' $files 
-perl -pi -w -e 's/stdout/stdoutput/g;' $files 
+perl -pi -w -e 's/fileno\(stdout/fileno\(Rcpp::Rcout/g;' $files
+perl -pi -w -e 's/stdout/stdoutput/g;' $files
 
 ## ====== Remove srand and rand, replace rand by R::runif(0, RAND_MAX)
-perl -pi -w -e 's/srand\(seed\)/\/\/srand\(seed\)/g;' $files 
-perl -pi -w -e 's/rand\(\)/R::runif\(0, RAND_MAX\)/g;' $files
 
-## ===== extra manual changes
-# lasreaderpipeon.cpp 		l96 		stdout > Rcpp::Rcout
-# laswriter.cpp			l132-197 	comment lines
-# laswriter_las.cpp		l96-105		comment lines
-# laswriter_qfit.cpp		l82-91		comment lines
-# laswriter_bin.cpp		l126-135	comment lines
-# bytestreamount_file.hpp 	l134		stdout > Rcpp::Rcout
+perl -pi -w -e 's/srand\(seed\)/\/\/srand\(seed\)/g;' $files
+perl -pi -w -e 's/rand\(\)/R::runif\(0, RAND_MAX\)/g;' $files
 
