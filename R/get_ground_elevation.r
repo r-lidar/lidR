44f3810183a534c8ec7bed5b70111123d61c75e0
JR <jean-romain.roussel.1@ulaval.ca>
2016-11-29 14:56:29 -0500

    Better support of error if no ground points
    Former-commit-id: fed1a23c00e80bab60e743a1407ef43514a337e5
    Former-commit-id: d5bd69a66da366984d7882176141fbe745b01708
    Former-commit-id: 0d9c4f429505ce7697d17a5c7323ae98b0a31e9e [formerly 33efb82203c414fb0d1152c25fa68b024c7bb2f5]
    Former-commit-id: 1725ac3544d16aa4ffddd09a5c8a775fd386d74c
    Former-commit-id: a0e8e5c084f7aada5447ffafaf01f9a208ffcb10
    Former-commit-id: b2d699cfed3106e94602d1c3e55942b135ae3687

--- R/get_ground_elevation.r
+++ R/get_ground_elevation.r
@@ -57,9 +57,13 @@ setMethod("get_ground_elevation", "LAS",
     fields = names(coord)
 
     if( !"X" %in% fields | !"Y" %in% fields)
-      stop("Parameter coord does not have a column named X or Y")
+      stop("Parameter coord does not have a column named X or Y",  call. = F)
 
-    ground = getGround(las)
+    ground = suppressWarnings(getGround(las))
+
+    if(is.null(ground))
+      stop("No ground points found. Impossible to normalize the point cloud.", call. = F)
+
     Zg = kknn::kknn(Z~X+Y, ground@data, coord, k = k, kernel = kernel, ...)$fitted.values
 
     return(Zg)
