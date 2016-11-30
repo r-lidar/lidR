ac842bf0956cc655faf24c0ef9793798d9310993
Jean-Romain <jromain.roussel@gmail.com>
2016-11-30 09:14:51 -0500

    Move fractal_dim in lasmetric.r
    Former-commit-id: 3235b259a980773e2bcf01518c20e50088dfb113
    Former-commit-id: 4ddd6068b3770834021b6ebf70dab3eff3fe7b24
    Former-commit-id: 5d726edcaedff48a0492f7b09170870789994de6 [formerly 8a0ab55ca70d9ff9c470a00ad9f1d6355a80c69e]
    Former-commit-id: f10d0f261237507ed7758537b7685abea5cb3e95
    Former-commit-id: 29e5b125255e704739658fabc6a621f25c8396c0
    Former-commit-id: 7b2b96d42c2bd72ab6e77052085e510ea4a434f2

--- R/lasmetrics.r
+++ R/lasmetrics.r
@@ -207,3 +207,58 @@ VCI = function(z, zmax, by = 1)
 
   return(entropy(z, by, zmax))
 }
+
+#' fractal_dimension
+#'
+#' Computes the fractal dimension of a surface. The fractal dimension is a measure
+#' of roughness.
+#'
+#' Fractal dimension compute the roughness based on the box counting method (see Taud and Parrot).
+#' If the input has an NA value, it returns NA. If the input is too small it returns NA.
+#' If the input matrix is not a square matrix, the function cuts the input matrix to create a square matrix.
+#' @param mtx numeric matrix that is the representation of a surface model
+#' @return numeric. A number between 0 and 3. 3 being the dimension of a volume
+#' @references Taud, H., & Parrot, J.-F. (2005). Mesure de la rugosite des MNT a l'aide de la dimension fractale. Geomorphologie : Relief, Processus, Environnement, 4, 327-338. http://doi.org/10.4000/geomorphologie.622
+#' @examples
+#' mtx = matrix(runif(100), 10, 10)
+#' fractal_dimension(mtx)
+#' @export fractal_dimension
+fractal_dimension = function(mtx)
+{
+  if( sum(is.na(mtx)) > 0 )
+    return(NA_real_)
+
+  size = min(dim(mtx))
+
+  if( size < 6)
+    return(NA_real_)
+
+  size = ifelse(size %% 2 == 0, size, size-1)
+
+  mtx = mtx[1:size, 1:size]
+
+  q = 1:size
+  q = q[size %% q == 0]
+
+  if(length(q) < 3)
+    return(as.numeric(NA))
+
+  nbbox = sapply(q, .countBox, mtx=mtx)
+
+  lm = stats::lm(log(nbbox) ~ log(q))
+
+  return(abs(as.numeric(stats::coefficients(lm)[2])))
+}
+
+.countBox = function(q, mtx)
+{
+	  rg <- (row(mtx)-1)%/%q+1
+    cg <- (col(mtx)-1)%/%q+1
+    rci <- (rg-1)*max(cg) + cg
+    N <- prod(dim(mtx))/(q^2)
+
+	  lasclip = lapply(1:N, function(x) mtx[rci==x])
+	  box = sapply(lasclip,max)/q
+
+	  return(sum(box))
+}
