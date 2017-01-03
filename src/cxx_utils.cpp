c6d02e9a1e56d7085253e214acac0fc4a278a378
Jean-Romain <jromain.roussel@gmail.com>
2017-01-03 12:04:05 -0500

    fast_table 30% faster
    Former-commit-id: 3f3cb4946987248a5a30594d9ae408ca3af4cf8d
    Former-commit-id: 02d52e70542a331b6b43b90dc5ca700dc56c6448
    Former-commit-id: bb0b9b48e4c79bf072b7c99a38a34c4b4dfb9e5e [formerly 9005bb7a32592fd397c1e2782768535f02d6b8b2]
    Former-commit-id: 411a8ad32fd4ea1e8ec1d3c6faae0811a1d7abbb
    Former-commit-id: e882824519533c78126bd5ba7cbb0b41a4ecd258
    Former-commit-id: 185314b745c593235d6a1e4873202209c9b795d3

--- src/cxx_utils.cpp
+++ src/cxx_utils.cpp
@@ -1,13 +1,12 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-
 // [[Rcpp::export]]
 IntegerVector fast_table(IntegerVector x, int size = 5)
 {
   IntegerVector tbl(size);
 
-  for (IntegerVector::iterator it = x.begin(); it != x.end(); ++it)
+  for (IntegerVector::iterator it = x.begin(), end = x.end() ; it != end ; ++it)
   {
     if (*it <= size)
       tbl(*it-1)++;
