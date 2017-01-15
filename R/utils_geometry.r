5882fc9a8883affcab650d58e35f975d361ac6b3
Jean-Romain <jromain.roussel@gmail.com>
2017-01-15 09:39:18 -0500

    File organization
    Former-commit-id: 0401043de1e5e0b94b06810059eee7070cdea622
    Former-commit-id: 6053920a88f1df63fd8cd21f871369e26b9366c5
    Former-commit-id: bd1310ad9e9103741c2d6ad3546a9d54bd90ab2f [formerly 7b9e7e62a707ba49c2c036b305ad3d6469efa9db]
    Former-commit-id: d5a7325ccd77dd173b30124aa7839b7c1dfb21ad
    Former-commit-id: 4bbfece7d0063335283a3b6881f99b9be1ac9738
    Former-commit-id: 7e982d30511a473883348aedc721a1a59168dae2

--- R/utils_geometry.r
+++ R/utils_geometry.r
@@ -25,18 +25,39 @@
 #
 # ===============================================================================
 
-round_any <- function(x, accuracy, f = round)
+convex_hull = function(x, y)
 {
-  f(x / accuracy) * accuracy
+	data = data.frame(x,y)
+	ch <- grDevices::chull(x,y)
+	coords <- data[c(ch, ch[1]), ]
+	rownames(coords) = NULL
+	return(coords)
 }
 
-make_grid = function(xmin, xmax, ymin, ymax, res)
+polygon_area = function(x, y)
 {
-  xo = seq(round_any(xmin, res), round_any(xmax, res), res) %>% round(2)
-  yo = seq(round_any(ymin, res), round_any(ymax, res), res) %>% round(2)
+  if (length(x) == 0 && length(y) == 0) return(0)
+  if (!is.numeric(x) || !is.numeric(y) ) stop("Arguments 'x' and 'y' must be real")
+  if (length(x) != length(y)) stop("Argument 'x' and 'y' must be of same size")
 
-  grid = expand.grid(X = xo, Y = yo)
-  data.table::setDT(grid)
+	area = 0;
+	j = length(x)
 
-  return(grid)
+	for (i in 1:j)
+	{
+		area = area + (x[j]+x[i])*(y[j]-y[i]);
+		j = i;
+	}
+
+	area  = abs(area*0.5)
+
+	return(area)
+}
+
+area = function(x, y)
+{
+  hull = convex_hull(x, y)
+  area = polygon_area(hull$x, hull$y)
+  area = round(area,1)
+  return(area)
 }
