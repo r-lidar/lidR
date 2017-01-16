d260bf1e54b55add0889dec6d8cff16e60df30f3
Jean-Romain <jromain.roussel@gmail.com>
2017-01-15 21:40:09 -0500

    More tests
    Former-commit-id: 7d9678809f816609dc1e3883ef3176601ecc34a2
    Former-commit-id: 575ff492d1bd9a9c2f8c62f469229a997e6fea39
    Former-commit-id: f82f65361f9921c10d0efd56dc7614cd636461aa [formerly d995fd201cf0f94b089afda04c4b942ba6f29cfd]
    Former-commit-id: bcf7cabb483fec94ab6f570b0b8587d8f2e715e9
    Former-commit-id: df3e1b421cbe6b9ee86746987f7779f2246cf8e6
    Former-commit-id: 69a1830f411537d7beef565c3d5bdeccfdf28e57

---
+++ tests/testthat/test-grid_metrics.R
@@ -0,0 +1,23 @@
+context("grid_metrics")
+
+las = lidR:::dummy_las(1000)
+
+test_that("grid_metrics works", {
+  x = grid_metrics(las, mean(Z))
+  expect_equal(dim(x), c(25,3))
+})
+
+test_that("grid_metrics debug mode works", {
+  expect_error(grid_metrics(las, LAD(Z)))
+})
+
+test_that("grid_metrics return an error if splitline and no flightlineID", {
+  expect_error(grid_metrics(las, mean(Z), splitlines = T))
+})
+
+las@data[, flightlineID := c(rep(1,500), rep(2,500))]
+
+test_that("grid_metrics splitline work", {
+  x = grid_metrics(las, mean(Z), splitlines = T)
+  expect_equal(dim(x), c(50,4))
+})
