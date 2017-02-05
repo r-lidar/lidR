452bd78be6d19f45eb9d61ff88fb3e5c81a127e6
Jean-Romain <jromain.roussel@gmail.com>
2017-02-05 14:00:39 -0500

    Rewrite
    Former-commit-id: 01f00881513c991bfcb62ae9a96b4e61a4a149f2
    Former-commit-id: 466445b013051c987bca398042787cba13ff1716
    Former-commit-id: 7fdbb7cf82b41202d629ed73c1fca4ce56da8c7d [formerly 915bb4e30c9cd2c1f9d73076f4c42dd59e69aa4a]
    Former-commit-id: 1c81b5b9bb1f4de61ec6e153d5b91f411bf8266e
    Former-commit-id: 3c28e37c74af0d90b7f6b32fab96e48d5318249e
    Former-commit-id: 83d6f854841d2504774de85f9e0aaa6b1a3a5b48

--- R/lasground.r
+++ R/lasground.r
@@ -2,7 +2,7 @@
 #
 # PROGRAMMERS:
 #
-# jean-romain.roussel.1@ulaval.ca  -  https://github.com/Jean-Romain/lidR
+# jean-romain.roussel.1@ulaval.ca  -  https:#github.com/Jean-Romain/lidR
 #
 # COPYRIGHT:
 #
@@ -21,19 +21,19 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# along with this program.  If not, see <http:#www.gnu.org/licenses/>
 #
 # ===============================================================================
 
 
 #' Classify points as ground or not ground
 #'
-#' Implements the Progressive Morphological Filter for segmentation of ground points.
+#' Implements a Progressive Morphological Filter for segmentation of ground points.
 #' The function updates the field \code{Classification}. The points classified as 'ground'
 #' get the value 2 according to las specifications (See the ASPRS documentation for the
-#' \href{http://www.asprs.org/a/society/committees/standards/LAS_1_4_r13.pdf}{LAS file format}).
-#' This function is a wrapper for the Progressive Morphological Filter implemented in the PCL
-#' library (Point Cloud Library, see reference).
+#' \href{http:#www.asprs.org/a/society/committees/standards/LAS_1_4_r13.pdf}{LAS file format}).
+#' This function is an implementation of Zhang et al. (2003) algorithm inspired from the
+#' implementation made in the PCL library (see references).
 #'
 #' @param .las a LAS object
 #' @param MaxWinSize maximum window size to be used in filtering ground returns (see references)
@@ -48,7 +48,7 @@
 #' @references
 #' Zhang, K., Chen, S. C., Whitman, D., Shyu, M. L., Yan, J., & Zhang, C. (2003). A progressive
 #' morphological filter for removing nonground measurements from airborne LIDAR data. IEEE
-#' Transactions on Geoscience and Remote Sensing, 41(4 PART I), 872–882. http://doi.org/10.1109/TGRS.2003.810682
+#' Transactions on Geoscience and Remote Sensing, 41(4 PART I), 872–882. http:#doi.org/10.1109/TGRS.2003.810682
 #' \cr\cr
 #' R. B. Rusu and S. Cousins, "3D is here: Point Cloud Library (PCL)," Robotics and Automation
 #' (ICRA), 2011 IEEE International Conference on, Shanghai, 2011, pp. 1-4. doi: 10.1109/ICRA.2011.5980567
@@ -67,89 +67,133 @@
 #' @importFrom data.table :=
 lasground = function(.las, MaxWinSize = 20, Slope = 1.0, InitDist = 0.5, MaxDist = 3.0, CellSize = 1.0, base = 2.0, exponential = TRUE)
 {
-  ex = extent(.las)
-  npt = dim(.las@data)[1]
-  .las@data[, id := 1:npt]
+  cloud = .las@data[, .(X,Y,Z)]
+  cloud[, idx := 1:dim(cloud)[1]]
 
-  f = function(Z, N) {
-    id = which.min(Z)
-    return(list(Z = Z[id], N = N[id]))
+  if("Classification" %in% names(.las@data))
+  {
+    nground = lidR:::fast_countequal(.las@data$Classification, 2)
+
+    if(nground > 0)
+    {
+      warning(paste0("Orginal dataset already contains ", nground, " ground points. These points were reclassified as 'unclassified' before to perform a new ground classification."), call. = FALSE)
+      .las@data[Classification == 2, Classification := 0]
+    }
+  }
+  else
+  {
+    .las@data[, Classification := 0]
   }
 
-  A = grid_metrics(.las, f(Z,id), CellSize)
-  A[, flag := 0]
+  idx = MorphologicalFilter(cloud, MaxWinSize, Slope, InitDist, MaxDist, CellSize, base, exponential)
 
-  k = 1:100
-  if(!exponential)
-    wk = 2*k*base + 1
-  else
-    wk = 2*base^k + 1
+  message(paste(length(idx), "ground points found."))
 
-  wk = wk[wk < MaxWinSize]
+  .las@data[idx, Classification := 2]
 
-  n = length(wk)
-  dtk = numeric(n)
-  dtk[1] = InitDist
-  dtk[2:n] = Slope * (wk[2:n] - wk[1:(n-1)]) * CellSize + InitDist
-  dtk[dtk > MaxDist] = MaxDist
+  return(invisible())
+}
 
-  for(i in 1:n)
+MorphologicalFilter = function(cloud, MaxWinSize, Slope, InitDist, MaxDist, CellSize, base, exponential)
+{
+  # Compute the series of window sizes and height thresholds
+  height_thresholds = c()
+  window_sizes = c()
+  iteration = 0
+  window_size = 0
+  height_threshold = 0
+
+  while (window_size <= MaxWinSize)
   {
-    w = wk[i]
-    dt = dtk[i]
+    # Determine the initial window size.
+    if (exponential)
+      window_size = CellSize * ( (2.0 * base^iteration) + 1 )
+    else
+      window_size = CellSize * (2.0 * (iteration+1) * base + 1)
 
-    for(j in 1:dim(A)[i])
+    # Calculate the height threshold to be used in the next iteration.
+    if (iteration == 0)
+      height_threshold = InitDist
+    else
+      height_threshold = Slope * (window_size - window_sizes[iteration]) * CellSize + InitDist
+
+    # Enforce max distance on height threshold
+    if (height_threshold > MaxDist)
+      height_threshold = MaxDist
+
+    window_sizes = append(window_sizes, window_size)
+    height_thresholds = append(height_thresholds, height_threshold)
+
+    iteration = iteration + 1
+  }
+
+  # Progressively filter ground returns using morphological open
+  for (i in 1:length(window_sizes))
+  {
+    # Create new cloud to hold the filtered results. Apply the morphological
+    # opening operation at the current window size.
+    cloud_f = applyMorphologicalOpening(cloud, window_sizes[i])
+
+    # Find indices of the points whose difference between the source and
+    # filtered point clouds is less than the current height threshold.
+    diff = cloud$Z - cloud_f$Z
+    indices = ifelse(diff < height_thresholds[i], TRUE, FALSE)
+
+    # Limit filtering to those points currently considered ground returns
+    cloud = cloud[indices]
+  }
+
+  return(cloud$idx)
+}
+
+applyMorphologicalOpening = function(cloud_in, resolution)
+{
+  cloud_temp = data.table::copy(cloud_in)
+  cloud_out  = data.table::copy(cloud_in)
+  n = dim(cloud_in)[1]
+
+  half_res = resolution / 2
+
+  for (p_idx in 1:n)
+  {
+    u = cloud_temp[p_idx]
+
+    minx = u$X - half_res
+    miny = u$Y - half_res
+    maxx = u$X + half_res
+    maxy = u$Y + half_res
+
+    pt1_indices = data.table::between(cloud_temp$X, minx, maxx) & data.table::between(cloud_temp$Y, miny, maxy)
+
+    if (sum(pt1_indices) > 0)
     {
-      id = RANN::nn2(A[, .(X,Y,Z)], A[i, .(X,Y,Z)], searchtype = "radius", k = 100, radius = w/2)$nn.idx
-      id = id[id > 0]
-
-      zf = min(A[id]$Z)
-
-      A[id, Zf := zf]
+      v = cloud_temp[pt1_indices]
+      min_pt = min(v$Z)
+      cloud_out[p_idx, Z := min_pt]
     }
   }
 
-  idground = A[flag == 0 & N > 0]$N
+  cloud_temp = data.table::copy(cloud_out)
 
-  .las@data[, Classification := 1]
-  .las@data[idground, Classification := 2]
-
-  return(invisible(NULL))
-}
-
-erosion = function(Z, w)
-{
-  w = w - 1
-  n = length(Z)
-  Zf = numeric(n)
-
-  for(i in 1:n)
+  for (p_idx in 1:n)
   {
-    pos = (i-w/2):(i+w/2)
-    pos = pos[pos > 0 & pos <= n ]
-    Zf[i] = min(Z[pos])
+    u = cloud_temp[p_idx]
+
+    minx = u$X - half_res
+    miny = u$Y - half_res
+    maxx = u$X + half_res
+    maxy = u$Y + half_res
+
+    pt2_indices = data.table::between(cloud_temp$X, minx, maxx) & data.table::between(cloud_temp$Y, miny, maxy)
+
+    if (sum(pt2_indices) > 0)
+    {
+      v = cloud_temp[pt2_indices]
+      max_pt = max(v$Z)
+      cloud_out[p_idx, Z := max_pt]
+    }
   }
 
-  return(Zf)
+  return(cloud_out)
 }
 
-dilatation = function(Z, w)
-{
-  w = w - 1
-  n = length(Z)
-  Zf = numeric(n)
-
-  for(i in 1:n)
-  {
-    pos = (i-w/2):(i+w/2)
-    pos = pos[pos > 0 & pos <= n ]
-    Zf[i] = max(Z[pos])
-  }
-
-  return(Zf)
-}
-
-opening = function(Z, w)
-{
-  dilatation(erosion(Z, w), w)
-}
