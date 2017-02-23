be7dac027624836909f7a74b1731d9770a97bbd5
Jean-Romain <jromain.roussel@gmail.com>
2017-02-22 19:08:25 -0500

    Faster polygon search

--- src/points_in_polygons.cpp
+++ src/points_in_polygons.cpp
@@ -47,10 +47,9 @@ using namespace Rcpp;
 bool point_in_polygon(NumericVector vertx, NumericVector verty, float pointx, float pointy)
 {
   bool c = false;
-  int i,j;
   int nvert = vertx.length();
 
-  for (i = 0, j = nvert-1 ; i < nvert ; j = i++)
+  for (int i = 0, j = nvert-1 ; i < nvert ; j = i++)
   {
     if( ((verty[i] > pointy) != (verty[j] > pointy)) && (pointx < (vertx[j]-vertx[i]) * (pointy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
       c = !c;
@@ -77,9 +76,7 @@ LogicalVector points_in_polygon(NumericVector vertx, NumericVector verty, Numeri
   LogicalVector c(npoints);
 
   for (i = 0 ; i < npoints ; i++)
-  {
     c[i] = point_in_polygon(vertx, verty, pointx[i], pointy[i]);
-  }
 
   return c;
 }
@@ -97,17 +94,38 @@ LogicalVector points_in_polygon(NumericVector vertx, NumericVector verty, Numeri
 // [[Rcpp::export]]
 IntegerVector points_in_polygons(Rcpp::List vertx, Rcpp::List verty, NumericVector pointx, NumericVector pointy)
 {
-  int i,j;
   int npoints = pointx.length();
   int nvert   = vertx.length();
   IntegerVector id(npoints);
 
-  for (i = 0 ; i < npoints ; i++)
+  // find bouding boxes to fast the agorithm
+  NumericVector bbxmin(nvert), bbxmax(nvert), bbymin(nvert), bbymax(nvert);
+  for(int i = 0 ; i < nvert ; i ++)
   {
-    for (j = 0 ; j < nvert ; j++)
+    bbxmin[i] = min(as<NumericVector>(vertx[i]));
+    bbxmax[i] = max(as<NumericVector>(vertx[i]));
+    bbymin[i] = min(as<NumericVector>(verty[i]));
+    bbymax[i] = max(as<NumericVector>(verty[i]));
+  }
+
+  for (int i = 0 ; i < npoints ; i++)
+  {
+    bool found = false;
+    int j = 0;
+
+    while (!found && j < nvert)
     {
+      // if point is inside the bounding box run point_in_polygon else it's useless (slightly faster)
+      if(bbxmin[j] <= pointx[i] || bbxmax[j] >= pointx[i] || bbymin[j] <= pointy[i] || bbymax[j] >= pointy[i])
+      {
         if(point_in_polygon(vertx[j], verty[j], pointx[i], pointy[i]))
+        {
           id[i] = j+1;
+          found = true;
+        }
+      }
+
+      j++;
     }
   }
 
