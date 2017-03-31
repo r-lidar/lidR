e1bf6bedfb583498678ada0f5a78ff80442cd96a
Jean-Romain <jromain.roussel@gmail.com>
2017-03-31 19:52:58 -0400

    .

--- src/init.c
+++ src/init.c
@@ -8,11 +8,7 @@
 */
 
 /* .Call calls */
-<<<<<<< HEAD
-extern SEXP lidR_algo_li2012(SEXP, SEXP, SEXP, SEXP, SEXP);
-=======
 extern SEXP lidR_algo_li2012(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
->>>>>>> 0b38ea5267a7eb08c56ccaafd24d9f0e723876a0
 extern SEXP lidR_fast_countbelow(SEXP, SEXP);
 extern SEXP lidR_fast_countequal(SEXP, SEXP);
 extern SEXP lidR_fast_countover(SEXP, SEXP);
@@ -26,11 +22,7 @@ extern SEXP lidR_points_in_polygon(SEXP, SEXP, SEXP, SEXP);
 extern SEXP lidR_points_in_polygons(SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-<<<<<<< HEAD
-    {"lidR_algo_li2012",          (DL_FUNC) &lidR_algo_li2012,          5},
-=======
     {"lidR_algo_li2012",          (DL_FUNC) &lidR_algo_li2012,          6},
->>>>>>> 0b38ea5267a7eb08c56ccaafd24d9f0e723876a0
     {"lidR_fast_countbelow",      (DL_FUNC) &lidR_fast_countbelow,      2},
     {"lidR_fast_countequal",      (DL_FUNC) &lidR_fast_countequal,      2},
     {"lidR_fast_countover",       (DL_FUNC) &lidR_fast_countover,       2},
