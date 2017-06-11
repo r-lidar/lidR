8db42f71ad99bba43280c6123d45f33524cd9044
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-06-11 12:15:04 -0400

    Review doc

--- R/option.r
+++ R/option.r
@@ -4,15 +4,18 @@ LIDROPTIONS <- settings::options_manager(verbose = FALSE, debug = FALSE, progres
                                                          debug    = settings::inlist(TRUE, FALSE),
                                                          progress = settings::inlist(TRUE, FALSE)))
 
-#' Set or get global options for lidR package
+#' Options Settings for the lidR package
+#'
+#' Allow the user to set and examine a variety of global options that affect the way in which
+#' lidR computes and displays its results.
 #'
 #' @param ... Option names to retrieve option values or \code{[key]=[value]} pairs to set options.
 #'
 #' @section Supported options:
 #' The following options are supported:
 #' \itemize{
-#'  \item{\code{verbose} (\code{logical}) Make the package talkative. }
-#'  \item{\code{progress} (\code{logical}) Display progress bars when avaible. }
+#'  \item{\code{verbose} (\code{logical}) Make the package "talkative". }
+#'  \item{\code{progress} (\code{logical}) Display progress bars when available. }
 #' }
 #'
 #' @examples
