1075fde5e071953682cf064bcfb7a4014743987b
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-07-07 16:10:51 -0400

    Improve rumple_index

--- tests/testthat/test-roughness.R
+++ tests/testthat/test-roughness.R
@@ -1,7 +1,7 @@
 context("roughness")
 
-test_that("Flat surfaces have a rumple index of 1", {
-  n = sample(20:50, 1)
+test_that("Flat surfaces have a rumple index of 1. Delaunay method", {
+  n = sample(10:20, 1)
   x = runif(n, 0, 100)
   y = runif(n, 0, 100)
   z = rep(10, n)
@@ -11,52 +11,25 @@ test_that("Flat surfaces have a rumple index of 1", {
   expect_equal(rumple_index(x, y, z*2), 1)
 })
 
-test_that("Rought surfaces have a rumple index > 1", {
-  n = sample(20:50, 1)
-  x = runif(n, 0, 100)
-  y = runif(n, 0, 100)
-  z = runif(n, 0, 10)
+test_that("Flat surfaces have a rumple index of 1. Jenness method", {
+  n = sample(10:20, 1)
+  r = expand.grid(X = 1:n, Y = 1:n)
+  r$Z = 1
+  data.table::setDT(r)
+  as.lasmetrics(r, 1)
 
-  expect_gt(rumple_index(x, y, z), 1)
-  expect_gt(rumple_index(x/10, y/5, z), 1)
-  expect_gt(rumple_index(x, y, z*2), 1)
+  expect_equal(rumple_index(r), 1)
 })
 
-test_that("The Roughter the surface the greater the rumple index", {
-  n = sample(20:50, 1)
+test_that("Rought surfaces have the good rumple index. Delaunay", {
+  n = sample(10:20, 1)
   x = runif(n, 0, 100)
   y = runif(n, 0, 100)
-  z1 = runif(n, 0, 10)
-  z3 = runif(n, 0, 30)
+  z = x
 
-  ri1 = rumple_index(x, y, z1)
-  ri3 = rumple_index(x, y, z3)
-  ri4 = rumple_index(x/2, y/2, z1)
+  expect_equal(rumple_index(x, y, z), sqrt(2))
 
-  expect_gt(ri3, ri1)
-  expect_gt(ri4, ri1)
-})
+  z = x + y
 
-
-test_that("rumple_index support CHM", {
-  las = lidR:::dummy_las(3000)
-  chm = las %>% grid_canopy(4)
-  rchm = as.raster(chm)
-
-  ri11 = rumple_index(chm)
-  ri12 = rumple_index(rchm)
-
-  expect_equal(ri11, ri12, tolerance = 0.01 * ri11)
-
-  las = lidR:::dummy_las(6000)
-  chm = las %>% grid_canopy(2)
-  rchm = as.raster(chm)
-
-  ri21 = rumple_index(chm)
-  ri22 = rumple_index(rchm)
-
-  expect_equal(ri21, ri22, tolerance = 0.01 * ri21)
-
-  expect_gt(ri21, ri11)
-  expect_gt(ri22, ri12)
+  expect_equal(rumple_index(x, y, z), sqrt(3))
 })
