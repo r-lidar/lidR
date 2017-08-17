bfd41686410c4fb027085ad114c8aa7f2eefa675
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-08-17 18:28:46 -0400

    Add constants

--- R/constant.R
+++ R/constant.R
@@ -1,3 +1,9 @@
 .LASVALIDFIELDS = c("X", "Y", "Z", "Intensity", "ReturnNumber", "NumberOfReturns", "ScanDirectionFlag", "EdgeOfFlightline", "Classification", "ScanAngle", "UserData", "gpstime", "PointSourceID", "R", "G", "B")
-.LIDRCIRCLE = 0
-.LIDRRECTANGLE = 1
+
+LIDRCIRCLE = 0
+LIDRRECTANGLE = 1
+
+LIDRBOTTOMBUFFER = 1
+LIDRLEFTBUFFER = 2
+LIDRTOPBUFFER = 3
+LIDRRIGHTBUFFER = 4
