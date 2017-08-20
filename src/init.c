4f9f9a65fafba0d8508b194b10377d8be8511143
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-08-20 11:35:55 -0400

    Update exports with new Rcpp routine

--- src/init.c
+++ src/init.c
@@ -8,40 +8,40 @@
 */
 
 /* .Call calls */
-extern SEXP lidR_algo_li2012(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_algo_li2012_v2(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_fast_countbelow(SEXP, SEXP);
-extern SEXP lidR_fast_countequal(SEXP, SEXP);
-extern SEXP lidR_fast_countover(SEXP, SEXP);
-extern SEXP lidR_fast_table(SEXP, SEXP);
-extern SEXP lidR_tinfo(SEXP, SEXP);
-extern SEXP lidR_itc_expandcrowns(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_itc_treetops(SEXP, SEXP);
-extern SEXP lidR_MorphologicalOpening(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_point_in_polygon(SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_points_in_polygon(SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_points_in_polygons(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_tsearch(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_knn(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP lidR_knnidw(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_algo_li2012(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_algo_li2012_v2(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_fast_countbelow(SEXP, SEXP);
+extern SEXP _lidR_fast_countequal(SEXP, SEXP);
+extern SEXP _lidR_fast_countover(SEXP, SEXP);
+extern SEXP _lidR_fast_table(SEXP, SEXP);
+extern SEXP _lidR_tinfo(SEXP, SEXP);
+extern SEXP _lidR_itc_expandcrowns(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_itc_treetops(SEXP, SEXP);
+extern SEXP _lidR_MorphologicalOpening(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_point_in_polygon(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_points_in_polygon(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_points_in_polygons(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_tsearch(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_knn(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _lidR_knnidw(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"lidR_algo_li2012",          (DL_FUNC) &lidR_algo_li2012,          7},
-    {"lidR_algo_li2012_v2",       (DL_FUNC) &lidR_algo_li2012_v2,       7},
-    {"lidR_fast_countbelow",      (DL_FUNC) &lidR_fast_countbelow,      2},
-    {"lidR_fast_countequal",      (DL_FUNC) &lidR_fast_countequal,      2},
-    {"lidR_fast_countover",       (DL_FUNC) &lidR_fast_countover,       2},
-    {"lidR_fast_table",           (DL_FUNC) &lidR_fast_table,           2},
-    {"lidR_tinfo",                (DL_FUNC) &lidR_tinfo,                2},
-    {"lidR_itc_expandcrowns",     (DL_FUNC) &lidR_itc_expandcrowns,     5},
-    {"lidR_itc_treetops",         (DL_FUNC) &lidR_itc_treetops,         2},
-    {"lidR_MorphologicalOpening", (DL_FUNC) &lidR_MorphologicalOpening, 5},
-    {"lidR_point_in_polygon",     (DL_FUNC) &lidR_point_in_polygon,     4},
-    {"lidR_points_in_polygon",    (DL_FUNC) &lidR_points_in_polygon,    4},
-    {"lidR_points_in_polygons",   (DL_FUNC) &lidR_points_in_polygons,   5},
-    {"lidR_tsearch",              (DL_FUNC) &lidR_tsearch,              6},
-    {"lidR_knn",                  (DL_FUNC) &lidR_knn,                  5},
-    {"lidR_knnidw",               (DL_FUNC) &lidR_knnidw,               6},
+    {"_lidR_algo_li2012",          (DL_FUNC) &_lidR_algo_li2012,          7},
+    {"_lidR_algo_li2012_v2",       (DL_FUNC) &_lidR_algo_li2012_v2,       7},
+    {"_lidR_fast_countbelow",      (DL_FUNC) &_lidR_fast_countbelow,      2},
+    {"_lidR_fast_countequal",      (DL_FUNC) &_lidR_fast_countequal,      2},
+    {"_lidR_fast_countover",       (DL_FUNC) &_lidR_fast_countover,       2},
+    {"_lidR_fast_table",           (DL_FUNC) &_lidR_fast_table,           2},
+    {"_lidR_tinfo",                (DL_FUNC) &_lidR_tinfo,                2},
+    {"_lidR_itc_expandcrowns",     (DL_FUNC) &_lidR_itc_expandcrowns,     5},
+    {"_lidR_itc_treetops",         (DL_FUNC) &_lidR_itc_treetops,         2},
+    {"_lidR_MorphologicalOpening", (DL_FUNC) &_lidR_MorphologicalOpening, 5},
+    {"_lidR_point_in_polygon",     (DL_FUNC) &_lidR_point_in_polygon,     4},
+    {"_lidR_points_in_polygon",    (DL_FUNC) &_lidR_points_in_polygon,    4},
+    {"_lidR_points_in_polygons",   (DL_FUNC) &_lidR_points_in_polygons,   5},
+    {"_lidR_tsearch",              (DL_FUNC) &_lidR_tsearch,              6},
+    {"_lidR_knn",                  (DL_FUNC) &_lidR_knn,                  5},
+    {"_lidR_knnidw",               (DL_FUNC) &_lidR_knnidw,               6},
     {NULL, NULL, 0}
 };
 
