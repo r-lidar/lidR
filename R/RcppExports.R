35eb5b8a1b4a1789c0b00a9ff290c161d646040c
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-08-20 11:35:55 -0400

    Update exports with new Rcpp routine
    Former-commit-id: e473d25359c7e46807c081a1f5f30806ee878d97
    Former-commit-id: 87297c0d0c4ff86796efb4607597023140535a27
    Former-commit-id: 0c2178bafa460b492a1deccb737e66cd2c904815 [formerly c42f1285ebf3aba20905cfbc09d8ebdf0f8c1289]
    Former-commit-id: 92fb72b0cee220fd7cafb8c357a91d7dcfff5607
    Former-commit-id: 74abaff2b43591caf8ec6342649a46c8dfe4d1c2
    Former-commit-id: b0c268f9dea080821a9252abe7e29038a272438a

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,66 +2,66 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 itc_treetops <- function(Canopy, searchWinSize) {
-    .Call(lidR_itc_treetops, Canopy, searchWinSize)
+    .Call(`_lidR_itc_treetops`, Canopy, searchWinSize)
 }
 
 itc_expandcrowns <- function(Canopy, Maxima, TRESHSeed, TRESHCrown, DIST) {
-    .Call(lidR_itc_expandcrowns, Canopy, Maxima, TRESHSeed, TRESHCrown, DIST)
+    .Call(`_lidR_itc_expandcrowns`, Canopy, Maxima, TRESHSeed, TRESHCrown, DIST)
 }
 
 algo_li2012 <- function(X, Y, Z, dt1, dt2, R, displaybar = FALSE) {
-    .Call(lidR_algo_li2012, X, Y, Z, dt1, dt2, R, displaybar)
+    .Call(`_lidR_algo_li2012`, X, Y, Z, dt1, dt2, R, displaybar)
 }
 
 algo_li2012_v2 <- function(X, Y, Z, dt1, dt2, R, displaybar = FALSE) {
-    .Call(lidR_algo_li2012_v2, X, Y, Z, dt1, dt2, R, displaybar)
+    .Call(`_lidR_algo_li2012_v2`, X, Y, Z, dt1, dt2, R, displaybar)
 }
 
 fast_table <- function(x, size = 5L) {
-    .Call(lidR_fast_table, x, size)
+    .Call(`_lidR_fast_table`, x, size)
 }
 
 fast_countequal <- function(x, t) {
-    .Call(lidR_fast_countequal, x, t)
+    .Call(`_lidR_fast_countequal`, x, t)
 }
 
 fast_countbelow <- function(x, t) {
-    .Call(lidR_fast_countbelow, x, t)
+    .Call(`_lidR_fast_countbelow`, x, t)
 }
 
 fast_countover <- function(x, t) {
-    .Call(lidR_fast_countover, x, t)
+    .Call(`_lidR_fast_countover`, x, t)
 }
 
 knn <- function(X, Y, x, y, k) {
-    .Call(lidR_knn, X, Y, x, y, k)
+    .Call(`_lidR_knn`, X, Y, x, y, k)
 }
 
 knnidw <- function(X, Y, Z, x, y, k) {
-    .Call(lidR_knnidw, X, Y, Z, x, y, k)
+    .Call(`_lidR_knnidw`, X, Y, Z, x, y, k)
 }
 
 MorphologicalOpening <- function(X, Y, Z, resolution, displaybar = FALSE) {
-    .Call(lidR_MorphologicalOpening, X, Y, Z, resolution, displaybar)
+    .Call(`_lidR_MorphologicalOpening`, X, Y, Z, resolution, displaybar)
 }
 
 point_in_polygon <- function(vertx, verty, pointx, pointy) {
-    .Call(lidR_point_in_polygon, vertx, verty, pointx, pointy)
+    .Call(`_lidR_point_in_polygon`, vertx, verty, pointx, pointy)
 }
 
 points_in_polygon <- function(vertx, verty, pointx, pointy) {
-    .Call(lidR_points_in_polygon, vertx, verty, pointx, pointy)
+    .Call(`_lidR_points_in_polygon`, vertx, verty, pointx, pointy)
 }
 
 points_in_polygons <- function(vertx, verty, pointx, pointy, displaybar = FALSE) {
-    .Call(lidR_points_in_polygons, vertx, verty, pointx, pointy, displaybar)
+    .Call(`_lidR_points_in_polygons`, vertx, verty, pointx, pointy, displaybar)
 }
 
 tinfo <- function(M, X) {
-    .Call(lidR_tinfo, M, X)
+    .Call(`_lidR_tinfo`, M, X)
 }
 
 tsearch <- function(x, y, elem, xi, yi, diplaybar = FALSE) {
-    .Call(lidR_tsearch, x, y, elem, xi, yi, diplaybar)
+    .Call(`_lidR_tsearch`, x, y, elem, xi, yi, diplaybar)
 }
 
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // itc_treetops
 IntegerMatrix itc_treetops(NumericMatrix Canopy, double searchWinSize);
-RcppExport SEXP lidR_itc_treetops(SEXP CanopySEXP, SEXP searchWinSizeSEXP) {
+RcppExport SEXP _lidR_itc_treetops(SEXP CanopySEXP, SEXP searchWinSizeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -19,7 +19,7 @@ END_RCPP
 }
 // itc_expandcrowns
 IntegerMatrix itc_expandcrowns(NumericMatrix Canopy, IntegerMatrix Maxima, double TRESHSeed, double TRESHCrown, double DIST);
-RcppExport SEXP lidR_itc_expandcrowns(SEXP CanopySEXP, SEXP MaximaSEXP, SEXP TRESHSeedSEXP, SEXP TRESHCrownSEXP, SEXP DISTSEXP) {
+RcppExport SEXP _lidR_itc_expandcrowns(SEXP CanopySEXP, SEXP MaximaSEXP, SEXP TRESHSeedSEXP, SEXP TRESHCrownSEXP, SEXP DISTSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -34,7 +34,7 @@ END_RCPP
 }
 // algo_li2012
 IntegerVector algo_li2012(NumericVector X, NumericVector Y, const NumericVector Z, double dt1, double dt2, double R, bool displaybar);
-RcppExport SEXP lidR_algo_li2012(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP dt1SEXP, SEXP dt2SEXP, SEXP RSEXP, SEXP displaybarSEXP) {
+RcppExport SEXP _lidR_algo_li2012(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP dt1SEXP, SEXP dt2SEXP, SEXP RSEXP, SEXP displaybarSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -51,7 +51,7 @@ END_RCPP
 }
 // algo_li2012_v2
 IntegerVector algo_li2012_v2(NumericVector X, NumericVector Y, const NumericVector Z, double dt1, double dt2, double R, bool displaybar);
-RcppExport SEXP lidR_algo_li2012_v2(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP dt1SEXP, SEXP dt2SEXP, SEXP RSEXP, SEXP displaybarSEXP) {
+RcppExport SEXP _lidR_algo_li2012_v2(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP dt1SEXP, SEXP dt2SEXP, SEXP RSEXP, SEXP displaybarSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -68,7 +68,7 @@ END_RCPP
 }
 // fast_table
 IntegerVector fast_table(IntegerVector x, int size);
-RcppExport SEXP lidR_fast_table(SEXP xSEXP, SEXP sizeSEXP) {
+RcppExport SEXP _lidR_fast_table(SEXP xSEXP, SEXP sizeSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -80,7 +80,7 @@ END_RCPP
 }
 // fast_countequal
 int fast_countequal(NumericVector x, double t);
-RcppExport SEXP lidR_fast_countequal(SEXP xSEXP, SEXP tSEXP) {
+RcppExport SEXP _lidR_fast_countequal(SEXP xSEXP, SEXP tSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -92,7 +92,7 @@ END_RCPP
 }
 // fast_countbelow
 int fast_countbelow(NumericVector x, double t);
-RcppExport SEXP lidR_fast_countbelow(SEXP xSEXP, SEXP tSEXP) {
+RcppExport SEXP _lidR_fast_countbelow(SEXP xSEXP, SEXP tSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -104,7 +104,7 @@ END_RCPP
 }
 // fast_countover
 int fast_countover(NumericVector x, double t);
-RcppExport SEXP lidR_fast_countover(SEXP xSEXP, SEXP tSEXP) {
+RcppExport SEXP _lidR_fast_countover(SEXP xSEXP, SEXP tSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -116,7 +116,7 @@ END_RCPP
 }
 // knn
 Rcpp::List knn(NumericVector X, NumericVector Y, NumericVector x, NumericVector y, int k);
-RcppExport SEXP lidR_knn(SEXP XSEXP, SEXP YSEXP, SEXP xSEXP, SEXP ySEXP, SEXP kSEXP) {
+RcppExport SEXP _lidR_knn(SEXP XSEXP, SEXP YSEXP, SEXP xSEXP, SEXP ySEXP, SEXP kSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -131,7 +131,7 @@ END_RCPP
 }
 // knnidw
 NumericVector knnidw(NumericVector X, NumericVector Y, NumericVector Z, NumericVector x, NumericVector y, int k);
-RcppExport SEXP lidR_knnidw(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP xSEXP, SEXP ySEXP, SEXP kSEXP) {
+RcppExport SEXP _lidR_knnidw(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP xSEXP, SEXP ySEXP, SEXP kSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -147,7 +147,7 @@ END_RCPP
 }
 // MorphologicalOpening
 NumericVector MorphologicalOpening(NumericVector X, NumericVector Y, NumericVector Z, double resolution, bool displaybar);
-RcppExport SEXP lidR_MorphologicalOpening(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP resolutionSEXP, SEXP displaybarSEXP) {
+RcppExport SEXP _lidR_MorphologicalOpening(SEXP XSEXP, SEXP YSEXP, SEXP ZSEXP, SEXP resolutionSEXP, SEXP displaybarSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -162,7 +162,7 @@ END_RCPP
 }
 // point_in_polygon
 bool point_in_polygon(NumericVector vertx, NumericVector verty, float pointx, float pointy);
-RcppExport SEXP lidR_point_in_polygon(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP) {
+RcppExport SEXP _lidR_point_in_polygon(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -176,7 +176,7 @@ END_RCPP
 }
 // points_in_polygon
 LogicalVector points_in_polygon(NumericVector vertx, NumericVector verty, NumericVector pointx, NumericVector pointy);
-RcppExport SEXP lidR_points_in_polygon(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP) {
+RcppExport SEXP _lidR_points_in_polygon(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -190,7 +190,7 @@ END_RCPP
 }
 // points_in_polygons
 IntegerVector points_in_polygons(Rcpp::List vertx, Rcpp::List verty, NumericVector pointx, NumericVector pointy, bool displaybar);
-RcppExport SEXP lidR_points_in_polygons(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP, SEXP displaybarSEXP) {
+RcppExport SEXP _lidR_points_in_polygons(SEXP vertxSEXP, SEXP vertySEXP, SEXP pointxSEXP, SEXP pointySEXP, SEXP displaybarSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -205,7 +205,7 @@ END_RCPP
 }
 // tinfo
 NumericMatrix tinfo(IntegerMatrix M, NumericMatrix X);
-RcppExport SEXP lidR_tinfo(SEXP MSEXP, SEXP XSEXP) {
+RcppExport SEXP _lidR_tinfo(SEXP MSEXP, SEXP XSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -217,7 +217,7 @@ END_RCPP
 }
 // tsearch
 IntegerVector tsearch(NumericVector x, NumericVector y, IntegerMatrix elem, NumericVector xi, NumericVector yi, bool diplaybar);
-RcppExport SEXP lidR_tsearch(SEXP xSEXP, SEXP ySEXP, SEXP elemSEXP, SEXP xiSEXP, SEXP yiSEXP, SEXP diplaybarSEXP) {
+RcppExport SEXP _lidR_tsearch(SEXP xSEXP, SEXP ySEXP, SEXP elemSEXP, SEXP xiSEXP, SEXP yiSEXP, SEXP diplaybarSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
