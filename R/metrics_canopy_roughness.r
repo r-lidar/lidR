29d88cbfb76f693067c51bd8def777d37f4e8e3d
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-09-04 16:58:21 -0400

    Coverage
    Former-commit-id: 92f816942160b02a090dc9b9251dcd4d14cc33a3
    Former-commit-id: c26cf8867c1a8c7679c425c818d58a04a6756ed2
    Former-commit-id: f8b0abc71dcc8226f9c76b96ddbde3aba713f295 [formerly d4bef35bc73a7e01dff18e7d322c2467922d5570]
    Former-commit-id: 837638fc5dbbacc39f7006d8a81c91b2ede710ff
    Former-commit-id: d4f479449624137db6e6fc1c7d95453074c4c922
    Former-commit-id: 4efde8a9d54d832eacfa8defb3a23dbb3c89add3

--- R/metrics_canopy_roughness.r
+++ R/metrics_canopy_roughness.r
@@ -25,75 +25,6 @@
 #
 # ===============================================================================
 
-# fractal_dimension
-#
-# Computes the fractal dimension of a surface. The fractal dimension is a measure
-# of roughness.
-#
-# Fractal dimension computes the roughness based on the box counting method (see Taud and Parrot).
-# If the input has an NA value, it returns NA. If the input is too small it returns NA.
-# If the input matrix is not a square matrix, the function cuts the input matrix to create a square matrix.
-# @param mtx numeric matrix that is the representation of a surface model
-# @return numeric. A number between 0 and 3. 3 being the dimension of a volume
-# @references Taud, H., & Parrot, J.-F. (2005). Mesure de la rugosite des MNT a l'aide de la dimension fractale. Geomorphologie : Relief, Processus, Environnement, 4, 327-338. http://doi.org/10.4000/geomorphologie.622
-# @examples
-# mtx = matrix(runif(100), 10, 10)
-# fractal_dimension(mtx)
-# @export fractal_dimension
-fractal_dimension = function(x, plot = FALSE)
-{
-  if (is(x, "RasterLayer"))
-    x = raster::as.matrix(x)
-  else if (!is.matrix(x))
-    stop("fractal_dimension: mtx should be a RasterLayer or a Matrix", call. = FALSE)
-
-  mtx = x
-
-  if ( sum(is.na(mtx)) > 0 )
-    return(NA_real_)
-
-  size = min(dim(mtx))
-
-  if ( size < 6)
-    return(NA_real_)
-
-  size = ifelse(size %% 2 == 0, size, size - 1)
-
-  mtx = mtx[1:size, 1:size]
-
-  q = 1:size
-  q = q[size %% q == 0]
-
-  if (length(q) < 3)
-    return(as.numeric(NA))
-
-  nbbox = sapply(q, countBox, mtx = mtx)
-
-  lm = stats::lm(log(nbbox) ~ log(q))
-
-  if (plot)
-  {
-    graphics::plot(log(nbbox) ~ log(q))
-    graphics::abline(lm)
-  }
-
-  return(abs(as.numeric(stats::coefficients(lm)[2])))
-}
-
-countBox = function(q, mtx)
-{
-	  rg  <- (row(mtx) - 1) %/% q + 1
-    cg  <- (col(mtx) - 1) %/% q + 1
-    rci <- (rg - 1) * max(cg) + cg
-    N   <- prod(dim(mtx))/(q^2)
-
-	  clip = lapply(1:N, function(x) mtx[rci == x])
-	  box = sapply(clip, max)/q
-
-	  return(sum(box))
-}
-
-
 #' Rumple index of roughness
 #'
 #' Computes the roughness of a surface as the ratio between its area and its
@@ -194,33 +125,70 @@ rumple_index.numeric <- function(x, y = NULL, z = NULL, ...)
   return(rumple)
 }
 
-# rumple_index_raster = function(x, res)
+# fractal_dimension
+#
+# Computes the fractal dimension of a surface. The fractal dimension is a measure
+# of roughness.
+#
+# Fractal dimension computes the roughness based on the box counting method (see Taud and Parrot).
+# If the input has an NA value, it returns NA. If the input is too small it returns NA.
+# If the input matrix is not a square matrix, the function cuts the input matrix to create a square matrix.
+# @param mtx numeric matrix that is the representation of a surface model
+# @return numeric. A number between 0 and 3. 3 being the dimension of a volume
+# @references Taud, H., & Parrot, J.-F. (2005). Mesure de la rugosite des MNT a l'aide de la dimension fractale. Geomorphologie : Relief, Processus, Environnement, 4, 327-338. http://doi.org/10.4000/geomorphologie.622
+# @examples
+# mtx = matrix(runif(100), 10, 10)
+# fractal_dimension(mtx)
+# @export fractal_dimension
+# fractal_dimension = function(x, plot = FALSE)
 # {
-#   x = cbind(0, x)
-#   x = rbind(0, x)
+#   if (is(x, "RasterLayer"))
+#     x = raster::as.matrix(x)
+#   else if (!is.matrix(x))
+#     stop("fractal_dimension: mtx should be a RasterLayer or a Matrix", call. = FALSE)
 #
-#   r = raster::raster(x)
+#   mtx = x
 #
-#   kernel = matrix(rep(0,9), ncol = 3)
-#   kernel[2,2] = 2
-#   kernel[2,3] = -1
-#   kernel[3,2] = -1
+#   if ( sum(is.na(mtx)) > 0 )
+#     return(NA_real_)
 #
-#   y = raster::focal(r, w = kernel, fun = sum, na.rm = TRUE)
-#   y = raster::as.matrix(y)
-#   y = y[,-1]
-#   y = y[-1,]
-#   y[, dim(y)[2]] = 0
-#   y[dim(y)[1], ] = 0
-#   y[is.na(x)] = NA
-#   y = abs(y)
+#   size = min(dim(mtx))
 #
-#   z = res^2 + res*y
+#   if ( size < 6)
+#     return(NA_real_)
 #
-#   return( sum(z, na.rm = T)/(sum(!is.na(z))*res^2) )
+#   size = ifelse(size %% 2 == 0, size, size - 1)
+#
+#   mtx = mtx[1:size, 1:size]
+#
+#   q = 1:size
+#   q = q[size %% q == 0]
+#
+#   if (length(q) < 3)
+#     return(as.numeric(NA))
+#
+#   nbbox = sapply(q, countBox, mtx = mtx)
+#
+#   lm = stats::lm(log(nbbox) ~ log(q))
+#
+#   if (plot)
+#   {
+#     graphics::plot(log(nbbox) ~ log(q))
+#     graphics::abline(lm)
+#   }
+#
+#   return(abs(as.numeric(stats::coefficients(lm)[2])))
 # }
-
-rumple_index_internal = function(x,y,z, options = "")
-{
-
-}
+#
+# countBox = function(q, mtx)
+# {
+# 	  rg  <- (row(mtx) - 1) %/% q + 1
+#     cg  <- (col(mtx) - 1) %/% q + 1
+#     rci <- (rg - 1) * max(cg) + cg
+#     N   <- prod(dim(mtx))/(q^2)
+#
+# 	  clip = lapply(1:N, function(x) mtx[rci == x])
+# 	  box = sapply(clip, max)/q
+#
+# 	  return(sum(box))
+# }
