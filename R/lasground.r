a9fc7aedefa2a28a5b3648ae1d2984080fc404f9
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-09-24 12:53:08 -0400

    lasground fixes and ready to get more algos
    Former-commit-id: 86da665a224b1b81078b261218d1d1c0d847bd8c
    Former-commit-id: 6563685adeb681cfcc2d3860ae550631abbd5524
    Former-commit-id: 883471b25bf7aacf7bea288a7b54b8a0da7e5b21 [formerly 04a0c3d9930ff3260b553ae1f1dd398593fa3768]
    Former-commit-id: b5cf8d51b8aefd43c8483eb19932a3905000e842
    Former-commit-id: 03da9e4eff78ab204159e3a26d9c559c76bf55bb
    Former-commit-id: ae1a949fc21cb1ed73d67f69fe8fb5aacd46105b

--- R/lasground.r
+++ R/lasground.r
@@ -28,23 +28,39 @@
 
 #' Classify points as ground or not ground
 #'
-#' Implements a Progressive Morphological Filter for segmentation of ground points.
-#' The function updates the field \code{Classification} of the input LAS object. The points
-#' classified as 'ground' are assigned a value of 2 according to las specifications (See the ASPRS
-#' documentation for the
+#' Implements algorithms for segmentation of ground points. The function updates the field
+#' \code{Classification} of the input LAS object. The points classified as 'ground' are
+#' assigned a value of 2 according to las specifications (See the ASPRS documentation for the
 #' \href{http://www.asprs.org/a/society/committees/standards/LAS_1_4_r13.pdf}{LAS file format}).
-#' This function is an implementation of the Zhang et al. (2003) algorithm (see reference)
 #'
-#' @param .las a LAS object
-#' @param MaxWinSize numeric. Maximum window size to be used in filtering ground returns (see references)
-#' @param Slope  numeric. Slope value to be used in computing the height thresholds (see references)
-#' @param InitDist numeric. Initial height above the parameterized ground surface to be considered a ground return (see references)
-#' @param MaxDist numeric. Maximum height above the parameterized ground surface to be considered a ground return (see references)
-#' @param CellSize numeric. Cell size
-#' @param ... Any additional specific parameters to be passed to the progressive morphological filter.
-#' These include:\cr
-#' - \code{exponential} logical. Default is TRUE.\cr
-#' - \code{base} numeric. Default is 2\cr
+#' @param las a LAS object
+#' @param algorithm character. The name of an algorithm. Currently \code{"pmf"} is supported
+#' (see related sections)
+#' @param ... Parameter for each avaible algorithm
+#' @param min_ws numeric. Minimum windows size to be used in filtering ground returns.
+#' This is the parameter \code{b} in Zhang et al. (2003) (see references)
+#' @param max_ws numeric. Maximum window size to be used in filtering ground returns in
+#' Zhang et al. (2003)
+#' @param min_th numeric. Initial threshold height above the parameterized ground surface
+#' to be considered a ground return. This is \code{dt0} in Zhang et al. (2003)
+#' @param max_th numeric. Maximum threshold height above the parameterized ground surface
+#' to be considered a ground return. This is \code{dhmax} in Zhang et al. (2003)
+#' @param s  numeric. Slope value to be used in computing the height thresholds sequence.
+#'  This is the parameter \code{s} in Zhang et al. (2003)
+#'
+#' @section Progressive morphological filter (PMF):
+#'
+#' This method is an implementation of the Zhang et al. (2003) algorithm (see reference).
+#' This is not a strict implementation of Zhang et al. This algorithm works at the raw point
+#' cloud level without any rasterization process. Therefore some parameter from the original
+#' description are no longer useful. The cell size \code{c} is not a requiered parameter.
+#' Therefore in the original paper the threshold sequence was:\cr\cr
+#' \eqn{th_k = s*(w_k - w_{k-1})*c + th_0}\cr\cr
+#' Now it is:\cr\cr
+#' \eqn{th_k = s*(w_k - w_{k-1}) + th_0}\cr\cr
+#' This method support an extra logical parameters \code{exponential} to change the method
+#' used to create the windows size sequence. Default is FALSE. See reference.
+#'
 #' @return Nothing. The original LAS object is updated by reference. In the 'Classification'
 #' column a value of 2 denotes ground according to LAS specifications.
 #' @references
@@ -54,56 +70,64 @@
 #' @export
 #' @examples
 #' LASfile <- system.file("extdata", "Topography.laz", package="lidR")
-#' las = readLAS(LASfile, XYZonly = TRUE)
+#' las = readLAS(LASfile, select = "xyz")
 #'
-#' lasground(las, MaxWinSize = 40, Slope = 1, MaxDist = 5, InitDist = 0.01, CellSize = 7)
+#' lasground(las, "pmf", min_ws = 2, max_ws = 40, min_th = 0.01, max_th = 5, slope = 1)
 #'
 #' plot(las, color = "Classification")
 #' @importFrom data.table :=
-lasground = function(.las, MaxWinSize = 20, Slope = 1.0, InitDist = 0.5, MaxDist = 3.0, CellSize = 1.0, ...)
+lasground = function(las, algorithm, ...)
+{
+  if (algorithm == "pmf")
+    lasground_pmf(las, ...)
+  else
+    stop("This algorithm does not exist.", call. = F)
+}
+
+#' @rdname lasground
+#' @export
+lasground_pmf = function(las, min_ws = 2, max_ws = 20, min_th = 0.5, max_th = 3.0, s = 1.0, ...)
 {
   . <- X <- Y <- Z <- Classification <- NULL
 
-  stopifnotlas(.las)
+  stopifnotlas(las)
 
   dots = list(...)
 
-  if (is.null(dots$base)) dots$base = 2
-  if (is.null(dots$exponential)) dots$exponential = TRUE
+  if (is.null(dots$exponential)) dots$exponential = FALSE
 
   exponential = dots$exponential
-  base = dots$base
 
-  cloud = .las@data[, .(X,Y,Z)]
+  cloud = las@data[, .(X,Y,Z)]
   cloud[, idx := 1:dim(cloud)[1]]
 
-  if ("Classification" %in% names(.las@data))
+  if ("Classification" %in% names(las@data))
   {
-    nground = fast_countequal(.las@data$Classification, 2)
+    nground = fast_countequal(las@data$Classification, 2)
 
     if (nground > 0)
     {
       warning(paste0("Orginal dataset already contains ", nground, " ground points. These points were reclassified as 'unclassified' before to perform a new ground classification."), call. = FALSE)
-      .las@data[Classification == 2, Classification := 0]
+      las@data[Classification == 2, Classification := 0]
     }
   }
   else
   {
-    .las@data[, Classification := 0]
+    las@data[, Classification := 0]
   }
 
   verbose("Progressive morphological filter...")
 
-  idx = ProgressiveMorphologicalFilter(cloud, MaxWinSize, Slope, InitDist, MaxDist, CellSize, base, exponential)
+  idx = ProgressiveMorphologicalFilter(cloud, min_ws, max_ws, min_th, max_th, s, exponential)
 
   message(paste(length(idx), "ground points found."))
 
-  .las@data[idx, Classification := 2]
+  las@data[idx, Classification := 2]
 
   return(invisible())
 }
 
-ProgressiveMorphologicalFilter = function(cloud, MaxWinSize, Slope, InitDist, MaxDist, CellSize, base, exponential)
+ProgressiveMorphologicalFilter = function(cloud, InitWinSize, MaxWinSize, InitDist, MaxDist, Slope, exponential)
 {
   # Compute the series of window sizes and height thresholds
   height_thresholds = c()
@@ -116,25 +140,33 @@ ProgressiveMorphologicalFilter = function(cloud, MaxWinSize, Slope, InitDist, Ma
   {
     # Determine the initial window size.
     if (exponential)
-      window_size = CellSize * ((2.0 * base^iteration) + 1 )
+      window_size = ((2.0 * InitWinSize^iteration) + 1)
     else
-      window_size = CellSize * (2.0 * (iteration + 1) * base + 1)
+      window_size = (2.0 * (iteration + 1) * InitWinSize + 1)
 
     # Calculate the height threshold to be used in the next iteration.
     if (iteration == 0)
       height_threshold = InitDist
     else
-      height_threshold = Slope * (window_size - window_sizes[iteration]) * CellSize + InitDist
+      height_threshold = Slope * (window_size - window_sizes[iteration]) + InitDist
 
     # Enforce max distance on height threshold
     if (height_threshold > MaxDist)
+    {
       height_threshold = MaxDist
+    }
 
-    window_sizes = append(window_sizes, window_size)
-    height_thresholds = append(height_thresholds, height_threshold)
+    if (window_size <= MaxWinSize)
+    {
+      window_sizes = append(window_sizes, window_size)
+      height_thresholds = append(height_thresholds, height_threshold)
+    }
 
     iteration = iteration + 1
   }
+
+  verbose("Windows sizes: ", window_sizes)
+  verbose("Height thresholds: ", height_thresholds)
 
   # Filter ground returns using a progressive morphological filter
   for (i in 1:length(window_sizes))
