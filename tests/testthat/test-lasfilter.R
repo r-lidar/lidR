32772ad7a6aa4ed4684131a666c64c87ea6a023a
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-09-27 16:39:43 -0400

    Test clip catalog

--- tests/testthat/test-lasfilter.R
+++ tests/testthat/test-lasfilter.R
@@ -1,6 +1,5 @@
 context("lasfilter")
 
-
 las = lidR:::dummy_las(500)
 
 test_that("filter first return works", {
@@ -33,29 +32,20 @@ test_that("clip circle works", {
   expect_true(extent(circ) <= raster::extent(40,60,40,60))
 })
 
-# test_that("clip generic with cuboid works", {
-#   mcube = matrix(c(10,10,0,50,50,20), nrow = 2, byrow = T)
-#   cube = lasclip(las, geometry = "cuboid", mcube)
-#   expect_true(extent(cube) <= raster::extent(10,50,10,50))
-#   expect_lt(max(cube@data$Z), 20)
-# })
-
-# test_that("clip generic with cuboid order the coordinates", {
-#   mcube = matrix(c(50,50,20,10,10,0), nrow = 2, byrow = T)
-#   cube = lasclip(las, geometry = "cuboid", mcube)
-#   expect_true(extent(cube) <= raster::extent(10,50,10,50))
-#   expect_lt(max(cube@data$Z), 20)
-# })
-
-# test_that("clip generic with rectangle works", {
-#   mrect = matrix(c(10,10,50,50), nrow = 2, byrow = T)
-#   rect  = lasclipRectangle(las, 10, 10, 50, 50)
-#   rect2 = lasclip(las, geometry = "rectangle", mrect)
-#   expect_equal(rect2, rect)
-# })
-
 test_that("filter on non matching data return null", {
   expect_warning(lasfilter(las, X > 200))
   expect_equal(suppressWarnings(lasfilter(las, X > 200)), NULL)
 })
 
+LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
+ctg = catalog(LASfile)
+
+test_that("clip circle works with a catalog", {
+  circ = lasclipCircle(ctg, 684850, 5017850, 10)
+  expect_true(extent(circ) <= raster::extent(684850-10,5017850-10,684850+10,5017850+10))
+})
+
+test_that("clip rectangle works with a catalog", {
+  rect = lasclipRectangle(ctg, 684850, 5017850, 684900, 5017900)
+  expect_true(extent(rect) <= raster::extent(684850, 5017850, 684900, 5017900))
+})
