a14a5317c90aac3ad8638f92636c2854c323d229
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-10-21 07:00:09 -0400

    Fix: raster depends on the starting point

--- tests/testthat/test-canopy.R
+++ tests/testthat/test-canopy.R
@@ -9,7 +9,7 @@ test_that("grid_canopy works", {
 
 test_that("grid_canopy works 2", {
 
-  dt = data.table::data.table(X = seq(0,10, 0.5), Y = seq(0,10, 0.5), Z = runif(20))
+  dt = data.table::data.table(X = seq(0,10, 0.5), Y = seq(0,10, 0.5), Z = runif(21))
   las = suppressWarnings(LAS(dt))
 
   chm = grid_canopy(las, 2)
@@ -43,3 +43,28 @@ test_that("grid_canopy works with subcircle", {
   chm = grid_canopy(las, 0.5, 10)
   expect_equal(dim(chm), c(16, 3))
 })
+
+test_that("grid_canopy create the same pixels independently of the starting point", {
+
+  las  = lidR:::dummy_las(100)
+  las2 = las
+  las3 = las
+
+  las2@data = rbind(las@data, list(-11.5,-30.25,20,1,1,1,1))
+  las3@data = rbind(las@data, list(-21.10,-18.75,20,1,1,1,1))
+
+  las2 = LAS(las2@data, las@header)
+  las3 = LAS(las3@data, las@header)
+
+  chm1 = grid_canopy(las2, 4)
+  chm2 = grid_canopy(las3, 4)
+
+  data.table::setorder(chm1, X, Y)
+  data.table::setorder(chm2, X, Y)
+
+  chm1 = chm1[-1]
+  chm2 = chm2[-1]
+
+  expect_equal(chm1, chm2)
+})
+
