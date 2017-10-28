44ea65958251cec80099a55acb3f70427f253741
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2017-10-28 08:08:16 -0400

    Split classes
    Former-commit-id: 10058d25c1ef6ccf962fcd8d66d5e5962ec7d555
    Former-commit-id: 2044fdeccbc09b3806e67bd8aa6617e52406781c
    Former-commit-id: 9c40ffc3dbd672c48adad10c0068be9dc1f1a9d6 [formerly 3604574b32cfc70170279d44ad6dee526519bc05]
    Former-commit-id: 297ac872721e8a9031aa372b94af78ed8f84aebc
    Former-commit-id: 2c21471aae24a6bde4a57e8958b18c2f5e7ff1e5
    Former-commit-id: 0f4c86d9e6f781ec297b2351291fb38fc6a97231

---
+++ src/BoundingBox.h
@@ -0,0 +1,16 @@
+#ifndef BBOX_H
+#define BBOX_H
+
+#include "Point.h"
+
+struct BoundingBox
+{
+	Point center, half_res;
+
+	BoundingBox();
+	BoundingBox(const Point,const Point);
+	bool contains(const Point&);
+	bool intersects(const BoundingBox&);
+};
+
+#endif //BBOX_H
