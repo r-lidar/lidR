a80ce4e1af226f405728cf6338b32e628a191abc
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-03-23 13:19:32 -0400

    Change onAttach message

--- R/zzz.r
+++ R/zzz.r
@@ -3,7 +3,7 @@
   if (interactive())
   {
     v = utils::packageVersion("lidR")
-    packageStartupMessage("lidR ", v, ". For help, for the most up-to-date version or to report a bug, visit https://github.com/Jean-Romain/lidR")
+    packageStartupMessage("lidR ", v, ". Help on <https://gis.stackexchange.com/>. Bug report on <https://github.com/Jean-Romain/lidR>.")
   }
 }
 
