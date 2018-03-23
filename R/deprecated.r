3ed51bfed14098584889ee03db2700cd753c35ca
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-03-23 13:15:02 -0400

    lasdecimate -> lasfilterdecimate
    Former-commit-id: 155329c1c7193a2835028b5b7e61390cdc80d4dd
    Former-commit-id: 6b9828b95a1117067e1ae6bc8707aa3725302e2b
    Former-commit-id: 430504098876c1d048004ee54c21d01e300404bb [formerly 1bd908661fe5048c52ee8f5337021ed5230bd380]
    Former-commit-id: cda7976dfcc837e87178940fc1f509f261c4c24e
    Former-commit-id: 42e686bf508ce1d047abff086e8bad0aa3099f16
    Former-commit-id: adf1310f382362268b3f6c3ebc15903c1342e137

--- R/deprecated.r
+++ R/deprecated.r
@@ -1,18 +1,16 @@
-# Deprecated function(s) in the lidR package
-#
-# These functions are provided for compatibility with older version of
-# the lidR package.  They may eventually be completely
-# removed.
-# @rdname lidR-deprecated
-# @name lidR-deprecated
-# @param ... Parameters to be passed to the modern version of the function
-# @docType package
-# @section Details:
-# \itemize{
-# \item{\code{\link{lasmetrics}} replace old \code{cloud_metrics}}
-# \item{\code{\link{area}} replace old \code{lasarea}}
-# }
-#
+#' Deprecated function(s) in the lidR package
+#'
+#' These functions are provided for compatibility with older version of
+#' the lidR package.  They may eventually be completely
+#' removed.
+#' @rdname lidR-deprecated
+#' @name lidR-deprecated
+#' @param ... Parameters to be passed to the modern version of the function
+#' @docType package
+lasdecimate <- function(...) {
+  .Deprecated("lasfilterdecimate")
+  lasfilterdecimate(...)
+}
 
 bool = function() { settings::inlist(TRUE, FALSE) }
 
