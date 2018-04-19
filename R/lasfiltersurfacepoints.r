9b0a32a5b85fbfdd7596ff3d8cc49b0405edeac4
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-04-19 16:10:56 -0400

    Remove smooth
    Former-commit-id: becb747125f09a8802b0b1a1069cd2508314382b
    Former-commit-id: 1d5e616b537e8c3fff617b4188cb3d16688b4875
    Former-commit-id: b2d8c959cc979c3b38331ab58ab238d38d0ca316 [formerly d6effae7b6114533670c8c4bad44807d92fba9be]
    Former-commit-id: c6a196c4f2ef9f388ee197d0a852077ee1a4bbdf
    Former-commit-id: 94785a888d32a109c75286025c18c944aa659d9b
    Former-commit-id: b060f07792c4fc9526253d4b084afc47effae431

--- R/lasfiltersurfacepoints.r
+++ R/lasfiltersurfacepoints.r
@@ -1,17 +1,15 @@
 #' Filter the surface points
 #'
 #' This routine creates a grid with a given resolution and filters the point cloud by selecting the
-#' highest elevation point within each cell.
+#' highest point within each cell.
 #'
 #' This function is different from \link{grid_canopy} even if the overall concept is exactly the same.
-#' \code{grid_canopy} attributes to each cell the elevation of the highest points and he point cloud is
+#' \code{grid_canopy} attributes to each cell the elevation of the highest points and the point cloud is
 #' rasterized. Here, there is no rasterization, the function have a point cloud as input and a point
 #' cloud as output. Coordinates and attributes are preserved.
 #'
 #' @param las A LAS object
 #' @param res numeric. The resolution of the grid used to filter the point cloud
-#' @param smooth numeric. Smooth the output point cloud using a moving average windows at the point
-#' level. This is the size of the moving windows. Default = 0 i.e. no smothing.
 #' @return A LAS object
 #' @export
 #' @family lasfilters
@@ -20,14 +18,11 @@
 #' las = readLAS(LASfile)
 #' subset = lasfiltersurfacepoints(las, 2)
 #' plot(subset)
-lasfiltersurfacepoints = function(las, res, smooth = 0)
+lasfiltersurfacepoints = function(las, res)
 {
   Z <- NULL
   by  = group_grid(las@data$X, las@data$Y, res)
   sub = las@data[las@data[, .I[which.max(Z)], by = by]$V1]
-
   las = LAS(sub, las@header)
-  Zsmoothed = C_lassmooth(las, smooth)
-  las@data[, Z := Zsmoothed]
   return(las)
 }
