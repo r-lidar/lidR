fa2e0bab33804c301d02ebbbad71ceb5e611c6a8
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-05-05 06:40:32 -0400

    Doc lasclip
    Former-commit-id: e300c87df41eb525b2f4706ef67cbd5a6b626102
    Former-commit-id: cefc2fb58290665055e3a0008a26efc0ee2c45fe
    Former-commit-id: 71ac9326fa9a42a3e4c61440db9c1a3a0a528a7d [formerly 0a5bdb71c9f0ab23248360a92f4d2a29cf54287d]
    Former-commit-id: b15ca9926ac9a8c744daabc653c65115c6eb3605
    Former-commit-id: ae46e23fa506e8b845fd711a437d12df7887d825
    Former-commit-id: f95373b0ad7789ebb647a37ac440ccc880e724c0

--- R/lasclip.r
+++ R/lasclip.r
@@ -1,6 +1,45 @@
+# ===============================================================================
+#
+# PROGRAMMERS:
+#
+# jean-romain.roussel.1@ulaval.ca  -  https://github.com/Jean-Romain/lidR
+#
+# COPYRIGHT:
+#
+# Copyright 2017-2018 Jean-Romain Roussel
+#
+# This file is part of lidR R package.
+#
+# lidR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+# ===============================================================================
+
+
 #' Clip LiDAR points
 #'
-#' Clip LiDAR points within a given geometry and convenient wrappers for most common geometries.
+#' Clip LiDAR points within a given geometry from a point cloud (\code{LAS} object) or a catalog
+#' (\code{LAScatalog} object)
+#'
+#' \code{lasclip} functions work both on \code{LAS} and \code{LAScatalog} objects. With a \code{LAS}
+#' object, the user firt read and load a point-cloud and then clip it to get a subset whithin a region
+#' of interest (ROI). With a \code{LAScatalog} object, the user extract the ROI whihout
+#' loading the whole point-cloud. This is faster and much memory efficient for extracting ROIs.
+#' \cr\cr
+#' Minor difference exist between the \code{LAS} and \code{LAScatalog} version of \code{lasclip}.
+#' For example the user can clip a \code{SpatialPolygonsDataFrame} from a \code{LAS} object but not
+#' from a \code{LAScatalog}. Also the option \code{inside = FALSE} is disable for \code{LAScatalog}
+#' object. These difference are generally justified by memory safety concerns.
 #'
 #' @param x An object of class \code{LAS} or \code{LAScatalog}.
 #' @param geometry a geometric object. Currently \code{Polygon} and \code{SpatialPolygonsDataFrame}
