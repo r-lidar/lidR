06b2114d56b47f22f7aca993bcba5fb60f6f41ea
Jasmin <Jasmin-S@users.noreply.github.com>
2018-05-25 11:04:54 -0400

    correction of bad convex hull storage

--- src/TreeCollection.h
+++ src/TreeCollection.h
@@ -122,24 +122,28 @@ template<typename T> double TreeCollection<T>::calculateTreeScores( int k )
 
 // Function that calculates convex hull areas for each selected tree in 'knnTreeID'
 
-template<typename T> void TreeCollection<T>::searchID_usingArea(std::vector<int> &knnTreeID, T &pointToSort, int &resultID, double &areaValue, boost::geometry::model::ring<point_t> &hull )
+template<typename T> void TreeCollection<T>::searchID_usingArea(std::vector<int> &knnTreeID, T &pointToSort, int &resultID, double &areaValue, boost::geometry::model::ring<point_t> &hull_out )
 {
   // Calcul de la premiere aire de la selection d'arbres
   double areaValue_diff = 0;
+  resultID = knnTreeID[0];
+  boost::geometry::model::ring<point_t> hull;
   areaValue_diff = treeStorage[knnTreeID[0]-1].testArea(pointToSort, areaValue, hull);   // page 100 eq. 3
+  hull_out.assign(hull.begin(), hull.end());
 
   // Comparaison avec les suivantes --> on garde la plus petite
-  // Attention -> pourquoi 0 quand calcul du convex hull avec trois points?
   double areaValueBis = 0;
   double areaValueBis_diff = 0;
 
   for (unsigned int i = 1; i < knnTreeID.size(); i++ )
   {
+    hull.clear();
     areaValueBis_diff = treeStorage[knnTreeID[i]-1].testArea( pointToSort, areaValueBis, hull );   // page 100 eq. 3
     if (areaValue_diff > areaValueBis_diff)
     {
       areaValue = areaValueBis;
       resultID = knnTreeID[i];
+      hull_out.assign(hull.begin(), hull.end());
     }
   }
 }
@@ -154,6 +158,7 @@ template<typename T> void TreeCollection<T>::searchID_usingDist( std::vector<int
   //Calcul de la premiere distance
   double distValueBis = 0;
   distValue = treeStorage[knnTreeID[0]-1].testDist( pointToSort );
+  resultID = knnTreeID[0];
   //Comparaison avec les suivantes --> on attribue le point à l'arbre avec la distance la plus petite
   for (unsigned int i = 1; i < knnTreeID.size(); i++ )
   {
