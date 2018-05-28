7552d5ecab43934f3c831a5957596d9cbb6caebd
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-05-28 12:50:40 -0400

    Update profiling
    Former-commit-id: 3afe888d5777d626046d01c67d702d79f41fa224
    Former-commit-id: 010e1aada90e2174cdf604455c8b2ea3d888322a [formerly 960a87c8e6528a665214f6a15e031c065d0100d7]
    Former-commit-id: dc11d474d67073fc7e4c8b0f3d84f2cb2f520eb8
    Former-commit-id: 1980299565f35f2eb08fcd527d6fcc1b359371b4 [formerly 33787a32592fb27d9f41db48962b91b368e921a2]
    Former-commit-id: b9a55f9adc79547b41e9c29f59048f89a7ff8b4c
    Former-commit-id: a758865ee14b583cc114b9bcaec469ddce680553
    Former-commit-id: 8b15796cfa40f8c48fbc40a85d9e046cc0da2545

--- profiling/lineprof-grid_canopy.r
+++ profiling/lineprof-grid_canopy.r
@@ -1,25 +1,18 @@
 library(lidR)
 library(profvis)
 
-las = readLAS("~/Documents/Haliburton dataset/Landscape LiDAR/CN_683_5016.las")
+las = readLAS("~/Documents/ALS data/Haliburton dataset/Landscape LiDAR/CN_683_5016.las")
 
-l = profvis(grid_canopy(las))
-l
+profvis(grid_canopy(las))
 
-# v1.2.1
-# major issues:
-
-l = profvis(grid_canopy(las, subcircle = 0.2))
-l
+profvis(grid_canopy(las, subcircle = 0.2))
 
 # v1.2.1
 # major issues:
 # - subcircled uses an insane amount of time (6 sec vs 1.4 sec for the aggregation)
-# - subcircled allocate a huge amount of memory.
 
-l = profvis(grid_tincanopy(las, thresholds = 0))
-l
+profvis(grid_tincanopy(las, thresholds = 0))
 
 # v1.2.1
 # major issues
-# - very slow with big file. delaunay: 6s, tsearch: 17s, tinfo: 1s
+# - very slow with big file. delaunay: 23s, tsearch: 64s, tinfo: 5s. total 97 secondes
