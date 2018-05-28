314cd45f4be618db4cec4e79b1f56533f24a6fb5
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-05-28 12:50:40 -0400

    Update profiling

--- profiling/lineprof-readLASr.R
+++ profiling/lineprof-readLASr.R
@@ -1,7 +1,7 @@
 library(lidR)
 library(profvis)
 
-l = profvis(readLAS("~/Documents/Haliburton dataset/Landscape LiDAR/CN_683_5016.las"))
+l = profvis(readLAS("~/Documents/ALS data/Haliburton dataset/Landscape LiDAR/CN_683_5016.las"))
 l
 
 # v1.3.0 las pulse takes the half the time (setorder to be exact).
