192a9df58a81c5f8d4e2445fec41d4663d435118
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-09-24 15:55:06 -0400

    New function lastransform
    Former-commit-id: 3d5e33ce9afca94ac09265f28bbac57fd78e2cfb
    Former-commit-id: 2a245357e2db8b761295639516aeb2813967c71d [formerly 6a3e6f4f14f47aeb683cefb2639a4936a32dcc8c]
    Former-commit-id: b76628c4a751af74095b5a3d396f4b7944933992
    Former-commit-id: 72ab463262ba4a471c4fb202621711f1315a79e5 [formerly 44689486221650702a77e67483950965a42fe5b2]
    Former-commit-id: 6763508b7c65727974b72adec7fd0fd654538d7d
    Former-commit-id: 58057692816e7722629e2c6ea04516760d60a73f
    Former-commit-id: f40eedc7eb87588d9d3f41720e80e52373be227a

---
+++ R/lastransform.r
@@ -0,0 +1,84 @@
+# ===============================================================================
+#
+# PROGRAMMERS:
+#
+# jean-romain.roussel.1@ulaval.ca  -  https:#github.com/Jean-Romain/lidR
+#
+# COPYRIGHT:
+#
+# Copyright 2016-2018 Jean-Romain Roussel
+#
+# This file is part of lidR R package.
+#
+# lidR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http:#www.gnu.org/licenses/>
+#
+# ===============================================================================
+
+
+#' Datum transformation for LAS object
+#'
+#' A version of \link[rgdal:spTransform]{spTrasform} for \link[lidR:LAS-class]{LAS} objects. r
+#' Returns transformed coordinates of an \code{LAS} object from the projection of the object to the
+#' the projection given in argument.
+#'
+#' @param las An object of class \link[lidR:LAS-class]{LAS}
+#'
+#' @param epsg integer. Unlike \code{spTransform} the EPSG code of the CRS should be prefereed for
+#' LAS objects. See also \link{epsg} to understand why. If missing \code{CRSobj} will be used instead.
+#' @param CRSobj logical. Object of class \link[sp:CRS-class]{CRS}, or of class character in which
+#' case it is converted to \link[sp:CRS-class]{CRS}.
+
+#'
+#' @return An object of class \link[lidR:LAS-class]{LAS} with coordinates XY transformed to the new
+#' coordinate reference system.
+#'
+#' @export
+#'
+#' @examples
+#' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
+#' las <- readLAS(LASfile, select = "xyzrn")
+#'
+#' head(las@data)
+#'
+#' las <- lastransform(las, 26918)
+lastransform = function(las, epsg, CRSobj = NULL)
+{
+  UseMethod("lastransform", las)
+}
+
+#' @export
+lastransform.LAS = function(las, epsg, CRSobj = NULL)
+{
+  if (!missing(epsg))
+    CRSobj = sp::CRS(paste0("+init=epsg:", epsg))
+
+  if (is.na(sp::proj4string(las)))
+    stop("No transformation possible from NA reference system")
+
+  spts = sp::SpatialPoints(las@data[, .(X,Y)])
+  sp::proj4string(spts) <- sp::proj4string(las)
+  spts = spTransform(spts, CRSobj)
+
+  las@data[["X"]] <- spts@coords[,1]
+  las@data[["Y"]] <- spts@coords[,2]
+
+  las <- lasupdateheader(las)
+
+  if (!missing(epsg))
+    epsg(las) <- epsg
+  else
+    sp::proj4string(las) <- sp::proj4string(spts)
+
+  return(las)
+}
