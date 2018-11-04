f79ff140cc228b051723c62c40fb3a5d0d05c7c2
Dave Auty <daveauty@gmail.com>
2018-11-04 15:42:20 -0700

    Update templates

--- man-roxygen/param-res-grid.R
+++ man-roxygen/param-res-grid.R
@@ -1,2 +1,2 @@
-#' @param res numeric. The resolution of the output \code{Raster}. Can optionnaly be a \code{RasterLayer}.
-#' In that case the the \code{RasterLayer} is used as layout.
+#' @param res numeric. The resolution of the output \code{Raster}. Can optionally be a \code{RasterLayer}.
+#' In that case the \code{RasterLayer} is used as the layout.
