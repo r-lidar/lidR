8a6913dcf9c013844f6e28a4adff34236110bb00
Dave Auty <daveauty@gmail.com>
2018-11-04 15:47:29 -0700

    Update more templates

--- man-roxygen/section-supported-option-lasfilter.R
+++ man-roxygen/section-supported-option-lasfilter.R
@@ -2,18 +2,18 @@
 #' Supported processing options for a \code{LAScatalog} (in bold). For more details see the
 #' \link[lidR:LAScatalog-class]{LAScatalog engine documentation}:
 #' \itemize{
-#' \item \strong{tiling_size}: How many data are loaded at once.
-#' \item buffer: This function guarantee a stric wall-to-wall continous output. The \code{buffer} option
-#' is no considered.
+#' \item \strong{tiling_size}: How much data is loaded at once.
+#' \item buffer: This function guarantee a strict wall-to-wall continous output. The \code{buffer} option
+#' is not considered.
 #' \item \strong{alignment}: Align the processed clusters
-#' \item \strong{cores}: How many cores are used. More cores means more data loaded at once.
+#' \item \strong{cores}: How many cores are used. More cores means more data is loaded at once.
 #' \item \strong{progress}: Displays a progression estimation.
 #' \item \strong{output_files*}: Mandatory because the output is likely to be too big to be returned
 #' in R and needs to be written in las/laz files. Supported templates are \code{XLEFT}, \code{XRIGHT},
-#' \code{YBOTTOM}, \code{YTOP}, \code{XCENTER}, \code{YCENTER} \code{ID} and, if tiling size equal to 0
+#' \code{YBOTTOM}, \code{YTOP}, \code{XCENTER}, \code{YCENTER} \code{ID} and, if tiling is size equal to 0
 #' (processing by file), \code{ORIGINALFILENAME}.
 #' \item \strong{laz_compression}: write \code{las} or \code{laz} files
-#' \item select: The function will write file equivalent to the original ones. Thus \code{select = "*"}
+#' \item select: The function will write files equivalent to the original ones. Thus \code{select = "*"}
 #' and cannot be changed.
 #' \item \strong{filter}: Read only points of interest.
 #' }
--- man-roxygen/section-supported-option-tree_detection.R
+++ man-roxygen/section-supported-option-tree_detection.R
@@ -2,17 +2,17 @@
 #' Supported processing options for a \code{LAScatalog} (in bold). For more details see the
 #' \link[lidR:LAScatalog-class]{LAScatalog engine documentation}:
 #' \itemize{
-#' \item \strong{tiling_size}: How many data are loaded at once.
-#' \item \strong{buffer*}: Mandatory to get a continuous output without edge effect. The buffer is
-#' always removed once processed and will never be returned neither in R nor in files.
+#' \item \strong{tiling_size}: How much data is loaded at once.
+#' \item \strong{buffer*}: Mandatory to get a continuous output without edge effects. The buffer is
+#' always removed once processed and will never be returned either in R or in files.
 #' \item \strong{alignment}: Align the processed clusters
 #' \item \strong{cores}: How many cores are used.
 #' \item \strong{progress}: Displays a progression estimation.
 #' \item \strong{output_files}: Supported templates are \code{\{XLEFT\}}, \code{\{XRIGHT\}},
-#' \code{\{YBOTTOM\}}, \code{\{YTOP\}}, \code{\{XCENTER\}}, \code{\{YCENTER\}} \code{\{ID\}} and, if tiling size equal to 0
+#' \code{\{YBOTTOM\}}, \code{\{YTOP\}}, \code{\{XCENTER\}}, \code{\{YCENTER\}} \code{\{ID\}} and, if tiling size is equal to 0
 #' (processing by file), \code{\{ORIGINALFILENAME\}}.
 #' \item \strong{laz_compression}: write \code{las} or \code{laz} files
-#' \item select: The function will write file equivalent to the original ones. Thus \code{select = "*"}
+#' \item select: The function will write files equivalent to the original ones. Thus \code{select = "*"}
 #' and cannot be changed.
 #' \item \strong{filter}: Read only points of interest.
 #' }
