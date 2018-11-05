3a73ee5ab0247f4670f05fd39d799a115bbe5396
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2018-11-05 09:11:06 -0500

    Regen doc
    Former-commit-id: be53358541e957835ad0b833e227fbb10e8a6bb8
    Former-commit-id: 1dfbb91f7765aaa32023fa1bb735e26e04efbd66 [formerly 4c649f3cbb1c4a2cfee6cbc603bf4a9bb6ab60fb]
    Former-commit-id: 3e9dcb35500a7079cdb562beb2cb1222a41c2b69
    Former-commit-id: 8d110d16a179f1f2ee3f8bc7ca4d0cffa2670527 [formerly d4c1d70488d540cb9513087309c6ff2b30721a2f]
    Former-commit-id: 4e6dc05406912baa78c3241b7a9a03f4223e299d
    Former-commit-id: f34153e7eda901289d8ca69b91a0a9b386c6e91f
    Former-commit-id: 52ab13b54f86cd53709a61b3da27ef453d452c26

--- man-roxygen/section-supported-option-lasupdater.R
+++ man-roxygen/section-supported-option-lasupdater.R
@@ -2,16 +2,16 @@
 #' Supported processing options for a \code{LAScatalog} (in bold). For more details see the
 #' \link[lidR:LAScatalog-class]{LAScatalog engine documentation}:
 #' \itemize{
-#' \item \strong{tiling_size}: How much data is loaded at once.
-#' \item \strong{buffer*}: Mandatory to get a continuous output without edge effects. The buffer is
-#' always removed once processed and will never be returned either in R or in files.
-#' \item \strong{alignment}: Align the processed clusters
+#' \item \strong{chunk size}: How much data is loaded at once.
+#' \item \strong{chunk buffer*}: Mandatory to get a continuous output without edge effects. The buffer
+#' is always removed once processed and will never be returned either in R or in files.
+#' \item \strong{chunk alignment}: Align the processed chunks.
 #' \item \strong{cores}: How many cores are used. More cores means more data is loaded at once.
 #' \item \strong{progress}: Displays a progression estimation.
 #' \item \strong{output_files*}: Mandatory because the output is likely to be too big to be returned
 #'  in R and needs to be written in las/laz files. Supported templates are \code{\{XLEFT\}}, \code{\{XRIGHT\}},
-#' \code{\{YBOTTOM\}}, \code{\{YTOP\}}, \code{\{XCENTER\}}, \code{\{YCENTER\}} \code{\{ID\}} and, if tiling size is equal to 0
-#' (processing by file), \code{\{ORIGINALFILENAME\}}.
+#' \code{\{YBOTTOM\}}, \code{\{YTOP\}}, \code{\{XCENTER\}}, \code{\{YCENTER\}} \code{\{ID\}} and, if
+#' chunk size is equal to 0 (processing by file), \code{\{ORIGINALFILENAME\}}.
 #' \item \strong{laz_compression}: write \code{las} or \code{laz} files
 #' \item select: The function will write files equivalent to the original ones. Thus \code{select = "*"}
 #' and cannot be changed.
