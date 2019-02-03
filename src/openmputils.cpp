65e6adaedd75ccdb33f2e29759f34b2dbe1ca19d
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-02-03 10:03:01 -0500

    Respect OMP_THREAD_LIMIT

--- src/openmputils.cpp
+++ src/openmputils.cpp
@@ -4,7 +4,9 @@
 int R_omp_get_max_threads()
 {
   #ifdef _OPENMP
-    int n = omp_get_max_threads();
+    int max = omp_get_max_threads();
+    int lim = omp_get_thread_limit();
+    int n = (max < lim) ? max : lim;
     return n;
   #else
     return -1;
