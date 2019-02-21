2d955cbd15844f049a9b198f489aa89452a7c988
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-02-21 08:57:57 -0500

    Fix #229

--- R/utils_define_constant.R
+++ R/utils_define_constant.R
@@ -1,33 +1,37 @@
-LIDRCIRCLE = 0L
-LIDRRECTANGLE = 1L
+LIDRCIRCLE            <- 0L
+LIDRRECTANGLE         <- 1L
 
-LIDRNOBUFFER = 0L
-LIDRBUFFER = 1L
-LIDRBOTTOMBUFFER = 1L
-LIDRLEFTBUFFER = 2L
-LIDRTOPBUFFER = 3L
-LIDRRIGHTBUFFER = 4L
+LIDRNOBUFFER          <- 0L
+LIDRBUFFER            <- 1L
+LIDRBOTTOMBUFFER      <- 1L
+LIDRLEFTBUFFER        <- 2L
+LIDRTOPBUFFER         <- 3L
+LIDRRIGHTBUFFER       <- 4L
 
-LASNONCLASSIFIED = 0L
-LASUNCLASSIFIED = 1L
-LASGROUND = 2L
-LASLOWVEGETATION = 3L
-LASMEDIUMVEGETATION = 4L
-LASHIGHVEGETATION = 5L
-LASBUILDING = 6L
-LASLOWPOINT = 7L
-LASKEYPOINT = 8L
-LASWATER = 9L
-LASFIELDS = c("X", "Y", "Z", "Intensity", "ReturnNumber", "NumberOfReturns", "ScanDirectionFlag", "EdgeOfFlightline", "Classification", "Synthetic_flag", "Keypoint_flag", "Withheld_flag", "ScanAngle", "UserData", "gpstime", "PointSourceID", "R", "G", "B")
+LASNONCLASSIFIED      <- 0L
+LASUNCLASSIFIED       <- 1L
+LASGROUND             <- 2L
+LASLOWVEGETATION      <- 3L
+LASMEDIUMVEGETATION   <- 4L
+LASHIGHVEGETATION     <- 5L
+LASBUILDING           <- 6L
+LASLOWPOINT           <- 7L
+LASKEYPOINT           <- 8L
+LASWATER              <- 9L
 
-ASYNC_RUN = -1L
-ASYNC_DONE = 0L
-ASYNC_OK = 1L
-ASYNC_NULL = 2L
-ASYNC_ERROR = 3L
-ASYNC_WARNING = 4L
+CHUNK_OK              <- 1L
+CHUNK_NULL            <- 2L
+CHUNK_ERROR           <- 3L
+CHUNK_WARNING         <- 4L
 
-CHUNK_OK = 1L
-CHUNK_NULL = 2L
-CHUNK_ERROR = 3L
-CHUNK_WARNING = 4L
+LASFIELDS             <- c("X", "Y", "Z", "Intensity",
+                           "ReturnNumber", "NumberOfReturns",
+                           "ScanDirectionFlag", "EdgeOfFlightline",
+                           "Classification",
+                           "Synthetic_flag", "Keypoint_flag",
+                           "Withheld_flag", "Overlap_flag",
+                           "ScanAngle", "ScanAngleRank",
+                           "ScannerChannel", "NIR",
+                           "UserData", "gpstime", "PointSourceID",
+                           "R", "G", "B")
+
