35b61976e039835b9c702ecb27208593a32dad15
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-04-05 09:47:31 -0400

    Skip test on solaris

--- tests/testthat/test-grid_terrain.R
+++ tests/testthat/test-grid_terrain.R
@@ -101,7 +101,7 @@ test_that("grid_terrain return the same both with LAScatalog and LAS", {
   error = error[error > 0.01]
 
   # skip unreprodutible error on CRAN with 32 bits arch and Solaris
-  if (!is.na(mean(error)) | Sys.info()['sysname'][[1]] == "SunOS")
+  if (!is.na(mean(error)))
   {
     expect_lt(length(error), raster::ncell(cdtm1)*0.002)
     expect_equal(mean(error), 0.048, tolerance = 0.001)
