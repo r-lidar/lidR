e4b7b1e7e3b4ab86c29ef8e699dbaf6e403ab27e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2020-03-17 10:32:25 -0400

    Update doc

--- R/segment_trees.R
+++ R/segment_trees.R
@@ -1,46 +1,58 @@
 #' Individual tree segmentation
 #'
 #' Individual tree segmentation with several possible algorithms. The returned point cloud has a new
-#' extra byte attribute named after the parameter \code{attribute} independently of the algorithm used.
+#' extra byte attribute named after the parameter `attribute` independently of the algorithm used.
 #'
 #' @template param-las
-#' @param algorithm function. An algorithm of individual tree segmentation. \code{lidR} has:
-#' \link{dalponte2016}, \link{watershed}, \link{mcwatershed}, \link{li2012} and \link{silva2016}.
-#' More experimental algorithms may be found in the package \href{https://github.com/Jean-Romain/lidRplugins}{lidRplugins}.
+#' @param algorithm function. An algorithm of individual tree segmentation. `lidR` has:
+#' \link{dalponte2016}, \link{watershed}, \link{li2012} and \link{silva2016}.
+#' More experimental algorithms may be found in the package [lidRplugins](https://github.com/Jean-Romain/lidRplugins).
 #' @param attribute character. The returned LAS object as a new extra byte attribute (in a new column).
-#' This parameter controls the name of the new attribute. Default is \code{"treeID"}.
+#' This parameter controls the name of the new attribute. Default is `"treeID"`.
 #' @param uniqueness character. A method to compute a unique ID. Can be 'incremental', 'gpstime' or
-  #' 'bitmerge'. See section 'Uniqueness'. This feature must be considered as 'experimental'.
+#' 'bitmerge'. See section 'Uniqueness'. This feature must be considered as 'experimental'.
 #'
 #' @section Uniqueness:
 #'
 #' By default the trees IDs are numbered from 1 to n, n being the number of tree found. The problem
 #' with such incremental numbering is that, while it ensure to get a unique ID for each tree in
 #' a given point-cloud it also gurantees to have duplicated tree IDs in different tiles or chunks when
-#' processing a \code{LAScatalog} because each file is processed independently of the other, potentially
-#' in parallel on different computers. Thus the index restart to 1 on each file or chunk. Worst, a
-#' tree that belongs exactly between 2 files will have two different IDs for its two halfs.
+#' processing a `LAScatalog` because each file is processed independently of the others and potentially
+#' in parallel on different computers. Thus the index always restart to 1 on each file or chunk. Worst,
+#' a tree that belongs exactly between 2 files will have two different IDs for its two halfs.
 #'
-#' This is why \code{segment_trees} was not able to process a \code{LAScatalog} until v2.3.0.
+#' This is why `segment_trees()` was not able to process a `LAScatalog` until v3.0.0. In v3.0.0 we
+#' introduced some uniqueness strategies that are all imperfect and that should be seen as experimental.
+#' Please report any troubleshooting. Using a uniqueness-safe strategy it ensures that trees from
+#' different files will not share the same IDs. Moreover it ensure that two halfs of a trees on the
+#' edge of a processing chunk will be attributed with the same ID.
 #'
 #' \describe{
-#' \item{incremental}{Number from 0 to n. This methods does not ensure unicity of the IDs.}
+#' \item{incremental}{Number from 0 to n. This methods **does not** ensure unicity of the IDs. This
+#' is the legacy method.}
 #' \item{gpstime}{This method uses the gpstime of the highest point of a tree (apex) to create a
 #' unique ID. This ID is not a integer but a 64 bits decimal number which is suboptimal but at
-#' least it is unique \bold{if tgpstime attribute is properly populated}. Moreover it ensure that
-#' two halfs of a trees on the edge of a processing chunk will be attributed with the same ID.}
+#' least it is exepected to be unique  **if the gpstime attribute is consistant accross files**.}
+#' If inconsistancies with gpstime are reported (for example gpstime records the week time and was
+#' reset to 0 in a coverage that takes more than a week to do) there is a (low) probability to get
+#' IDs attribution errors.
 #' \item{bitmerge}{This method uses the XY coordinates of the highest point of a tree (apex) to
-#' create a single number with a bitwise operation. First XY are converted back to integers with the
-#' scales and offsets given in the header of point-cloud. Then the ID is computed by X * 2^32 + Y
-#' to combine all the information. For example if X = 10 and Y = 25 the ID is 21474836505.
-#' Such methods also returns a 64 bits decimal number but this time the number corresponds to an
-#' integer that is guranteed to be unique \bold{if all files have the same offsets and scale factors}.}
+#' create a single number with a bitwise operation. First XY are converted back to integers using the
+#' scales and offsets of point-cloud. Then the ID is computed with X * 2^32 + Y
+#' to combine twice 32 bits of information into a 64 bits number. For example if the apex is at (10.32, 25.64)
+#' with a scale factor of 0.01 and an offset of 0 the integer coordinates are X = 1032 and Y = 2564
+#' and the ID is 4432406252036. Such methods returns a 64 bits integer but because 64 bits integer do
+#' not exist in R it is converted to 64 bits decimal number that is guaranteed to be unique
+#' **if all files have the same offsets and scale factors**.}
 #' }
 #'
-#' All the proposed options are suboptimal because they either do not guarantee uniqueness or they
-#' implies that IDs are based on non integers meaningless numbers. But at least it works.
+#' All the proposed options are suboptimal because they either do not guarantee uniqueness in all cases
+#' (inconsitancies in the collection of files) and they implies that IDs are based on non integers
+#' meaningless numbers. But at least it should work in simple cases.
 #'
 #' @template LAScatalog
+#'
+#' @template section-supported-option-lasupdater
 #'
 #' @template return-lasupdater-las-lascatalog
 #'
@@ -53,6 +65,7 @@
 #' # Using Li et al. (2012)
 #' las <- segment_trees(las, li2012(R = 3, speed_up = 5))
 #' plot(las, color = "treeID")
+#' @md
 segment_trees = function(las, algorithm, attribute = "treeID", uniqueness = 'incremental')
 {
   UseMethod("segment_trees", las)
