81c00d15398575d1991646756db0af74bf607565
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2020-06-01 09:52:14 -0400

    Add unit tests

--- tests/testthat/test-lasmergespatial.R
+++ tests/testthat/test-lasmergespatial.R
@@ -3,7 +3,7 @@ context("lasmergespatial")
 LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
 shapefile_dir <- system.file("extdata", package = "lidR")
 
-lidar <- readLAS(LASfile, select = "xyz", filter = "-thin_with_grid 2")
+lidar <- readLAS(LASfile, select = "xyzi", filter = "-thin_with_grid 2")
 lakes <- rgdal::readOGR(shapefile_dir, "lake_polygons_UTM17", verbose = FALSE)
 
 test_that("lasmergespatial works with SpatialPolygonsDataFrame", {
@@ -84,6 +84,17 @@ test_that("lasmergespatial never fails (sf)", {
   expect_true(all(is.na(lidar$id)))
 })
 
+test_that("lasmergespatial do not fail with 1 point (#347)", {
+
+  one_out <- filter_poi(lidar, Z >= 29.97)
+  one_in <- filter_poi(lidar, Intensity == 320)
+
+  one_out <- merge_spatial(one_out, lakes)
+  one_in <- merge_spatial(one_in, lakes)
+
+  expect_equal(one_out$id, NA_integer_)
+  expect_equal(one_in$id, 1L)
+})
 
 test_that("lasmergespatial works with SpatialPolygons", {
 
