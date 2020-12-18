ef8509877bae821b672dec8a9ec9c1692845f045
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2020-12-17 21:29:50 -0500

    Remove link to gdalUtils documentation

--- man-roxygen/return-grid-Layer.R
+++ man-roxygen/return-grid-Layer.R
@@ -1,3 +1,3 @@
 #' @return A \code{RasterLayer} containing a numeric value in each cell. If the \code{RasterLayer}s
 #' are written on disk when running the function with a \code{LAScatalog}, a virtual raster mosaic is
-#' returned (see \link[gdalUtils:gdalbuildvrt]{gdalbuildvrt})
+#' returned (see \href{https://gdal.org/programs/gdalbuildvrt.html}{gdalbuildvrt})
--- man-roxygen/return-grid-LayerBrick.R
+++ man-roxygen/return-grid-LayerBrick.R
@@ -1,3 +1,3 @@
 #' @return A \code{RasterLayer} or a \code{RasterBrick} containing a numeric value in each cell. If the
 #' \code{RasterLayer}s are written on disk when running the function with a \code{LAScatalog}, a
-#' virtual raster mosaic is returned (see \link[gdalUtils:gdalbuildvrt]{gdalbuildvrt})
+#' virtual raster mosaic is returned (see \href{https://gdal.org/programs/gdalbuildvrt.html}{gdalbuildvrt})
