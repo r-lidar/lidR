3ae4da09339c47ba4b9b2802c36412a7878546e8
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-01-25 05:20:13 -0500

    skip writelax on CRAN

--- tests/testthat/test-catalog_laxindex.R
+++ tests/testthat/test-catalog_laxindex.R
@@ -1,6 +1,7 @@
 context("catalog_laxindex")
 
 test_that("catalog_laxindex works", {
+  skip_on_cran() # ASAN/USBAN to fix in rlas
 
   ctg <- random_2files_250points
   expect_error(lidR:::catalog_laxindex(ctg), NA)
