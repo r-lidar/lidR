36586ac7330108482920983d71958d2a3f17929b
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-11-12 08:45:52 -0500

    plugins

---
+++ R/plugins.R
@@ -0,0 +1,108 @@
+LIDRDSM <- "DigitalSurfaceModel"
+LIDRSPI <- "SpatialInterpolation"
+LIDRGND <- "GroundSegmentation"
+LIDRITD <- "IndividualTreeDetection"
+LIDRDEC <- "PointCloudDecimation"
+LIDRSHP <- "ShapeDetection"
+LIDRSNG <- "SnagsSegmentation"
+LIDRITS <- "IndividualTreeSegmentation"
+LIDRNIT <- "normalize_intensity"
+LIDRTRK <- "SensorTracking"
+LIDROUT <- "NoiseSegmentation"
+
+LIDRCONTEXTDSM <- "rasterize_canopy"
+LIDRCONTEXTSPI <- c("normalize_height", "rasterize_terrain", "p2r", "spatial_interpolation")
+LIDRCONTEXTGND <- "classify_ground"
+LIDRCONTEXTOUT <- "classify_noise"
+LIDRCONTEXTITD <- "locate_trees"
+LIDRCONTEXTDEC <- "decimate_points"
+LIDRCONTEXTSHP <- "segment_shapes"
+LIDRCONTEXTSNG <- "segment_snags"
+LIDRCONTEXTITS <- "segment_trees"
+LIDRCONTEXTNIT <- "normalize_intensity"
+LIDRCONTEXTTRK <- "track_sensor"
+
+LIDRALGORITHM <- "lidRAlgorithm"
+LIDRALGORITHMGENERIC = c(LIDRALGORITHM, "ANY", "function")
+LIDRALGORITHMOPENMP <- "omp"
+LIDRALGORITHMPOINTCLOUDBASED <- "PointCloudBased"
+LIDRALGORITHMRASTERBASED <- "RasterBased"
+LIDRALGORITHMDEC <- LIDRALGORITHMDSM <- LIDRALGORITHMGND <- LIDRALGORITHMITD <- LIDRALGORITHMITS <- LIDRALGORITHMNIT <- LIDRALGORITHMSNG <- LIDRALGORITHMTRK <- LIDRALGORITHMSHP <- LIDRALGORITHMSPI <- LIDRALGORITHMOUT <- LIDRALGORITHMGENERIC
+LIDRALGORITHMDEC[2] <- LIDRDEC
+LIDRALGORITHMDSM[2] <- LIDRDSM
+LIDRALGORITHMGND[2] <- LIDRGND
+LIDRALGORITHMITD[2] <- LIDRITD
+LIDRALGORITHMITS[2] <- LIDRITS
+LIDRALGORITHMNIT[2] <- LIDRNIT
+LIDRALGORITHMSNG[2] <- LIDRSNG
+LIDRALGORITHMTRK[2] <- LIDRTRK
+LIDRALGORITHMSHP[2] <- LIDRSHP
+LIDRALGORITHMSPI[2] <- LIDRSPI
+LIDRALGORITHMOUT[2] <- LIDROUT
+
+plugin <- function(f, class_t, omp = FALSE)
+{
+  if (omp) class_t <- c(class_t, LIDRALGORITHMOPENMP)
+  class(f) <- class_t
+  return(f)
+}
+
+
+#' Plugin system
+#'
+#' Tools to build plugin functions for lidR
+#'
+#' @param f a function
+#' @param omp logical is the function natively parallized with OpenMP
+#'
+#' @name plugins
+#' @rdname plugins
+#' @examples
+#' \dontrun{
+#' mba <- function(n = 1, m = 1, h = 8, extend = TRUE) {
+#'   f <- function(las, where) {
+#'     res <- MBA::mba.points(las@data, where, n, m , h, extend)
+#'     return(res$xyz.est[,3])
+#'   }
+#'
+#'   f <- plugin_dtm(f)
+#'   return(f)
+#' }
+#' }
+NULL
+
+#' @export
+#' @rdname plugins
+plugin_dsm      <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMDSM, omp))
+
+#' @export
+#' @rdname plugins
+plugin_dtm      <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMSPI, omp))
+
+#' @export
+#' @rdname plugins
+plugin_gnd      <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMGND, omp))
+
+#' @export
+#' @rdname plugins
+plugin_itd      <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMITD, omp))
+
+#' @export
+#' @rdname plugins
+plugin_its      <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMITS, omp))
+
+#' @export
+#' @rdname plugins
+plugin_decimate <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMDEC, omp))
+
+#' @export
+#' @rdname plugins
+plugin_shape    <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMSHP, omp))
+
+#' @export
+#' @rdname plugins
+plugin_snag     <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMSNG, omp))
+
+#' @export
+#' @rdname plugins
+plugin_track    <- function(f, omp = FALSE) return(plugin(f, LIDRALGORITHMTRK, omp))
