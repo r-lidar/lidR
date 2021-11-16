cf249648dbd9a54bddc3106c9feb7d723516239e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-11-16 12:46:27 -0500

    Fix output

--- R/metrics_polygon.R
+++ R/metrics_polygon.R
@@ -10,6 +10,8 @@ polygon_metrics.LAS = function(las, func, geometry, ...)
 {
   stopifnot(is(geometry, "sf") | is(geometry, "sfc"))
   geometry <- sf::st_geometry(geometry)
+  stopifnot(is(geometry, "sfc_POLYGON") | is(geometry, "sfc_MULTIPOLYGON"))
   M <- template_metrics(las, func, geometry, ...)
-  return(geometry)
+  M <- sf::st_set_geometry(M, geometry)
+  return(M)
 }
