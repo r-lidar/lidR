6c0487e2df1b4add31b0d179657a824ad7ae4266
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-11-18 09:31:54 -0500

    st_coordinates

--- R/st_coordinates.R
+++ R/st_coordinates.R
@@ -1,25 +1,36 @@
 #' Coordinates of LAS* object in a matrix form
 #'
 #' Retrieve coordinates of a LAS* object in matrix form. It creates a copy of the coordinates
+#' because of the coercion from `data.frame` to `matrix`. This function inherits `sf::st_coordinates`
 #'
 #' @template param-las
+#' @param z bool. Return XY or XYZ matrix
 #' @param ... unused.
 #'
 #' @return matrix
 #'
 #' @name st_coordinates
+#' @rdname st_coordinates
+#' @importFrom sf st_coordinates
 #' @md
-#' @noRd
-st_coordinates <- function(x, ...)
+#' @examples
+#' LASfile <- system.file("extdata", "example.laz", package="rlas")
+#' las <- readLAS(LASfile)
+#' sf::st_coordinates(las)
+NULL
+
+#' @rdname st_coordinates
+#' @export
+st_coordinates.LAS <- function(x, z = TRUE, ...)
 {
-  UseMethod("st_coordinates", x)
+  if (isTRUE(z))
+    return(as.matrix(coordinates3D(x)))
+  else
+    return(as.matrix(coordinates(x)))
 }
 
-st_coordinates.LAS <- function(x, ...)
-{
-  return(as.matrix(coordinates3D(x)))
-}
-
+#' @rdname st_coordinates
+#' @export
 st_coordinates.LAScatalog <- function(x, ...)
 {
   return(sf::st_coordinates(x@data))
