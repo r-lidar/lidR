6f1931a81d403c7c48698841670e1e18f2dd922e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2021-11-19 08:54:06 -0500

    concave hull

--- R/catalog_boundaries.R
+++ R/catalog_boundaries.R
@@ -1,27 +1,22 @@
 #' Computes the polygon that encloses the points
 #'
 #' Computes the polygon that encloses the points. It reads all the files one by one and computes a
-#' concave hull using the \link{concaveman} function. When all the hulls are computed it updates the
+#' concave hull using the \link{st_concave_hull} function. When all the hulls are computed it updates the
 #' LAScatalog to set the true polygons instead of the bounding boxes.
 #'
 #' @param ctg A LAScatalog
-#' @param concavity,length_threshold see \link{concaveman}
+#' @param ... propagated to \link{st_concave_hull}
 #'
 #' @return A LAScatalog with true boundaries
 #' @export
 #' @examples
 #' LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
 #' ctg <- readLAScatalog(LASfile, filter = "-drop_z_below 0.5")
-#' ctg2 <- catalog_boundaries(ctg, 1, 15)
+#' ctg2 <- catalog_boundaries(ctg, concavity = 1, length_threshold = 15)
 #' plot(ctg)
 #' plot(ctg2, add = TRUE)
-catalog_boundaries = function(ctg, concavity = 5, length_threshold = 5)
+catalog_boundaries = function(ctg, ...)
 {
-  f = function(las, bbox, concavity, length_threshold) {
-    p <- st_concave_hull(las, concavity = concavity, length_threshold = length_threshold)
-    return(p)
-  }
-
   if (is_lascatalog_v3(ctg)) ctg <- lascatalog_v3_repair(ctg)
 
   opt_chunk_buffer(ctg) <- 0
@@ -29,11 +24,9 @@ catalog_boundaries = function(ctg, concavity = 5, length_threshold = 5)
   opt_select(ctg) <- "xyz"
   opt_output_files(ctg) <- ""
   opt_independent_files(ctg) <- TRUE
-  options <- list(autoread = TRUE)
-  res <- catalog_apply(ctg, f, concavity = concavity, length_threshold = length_threshold, .options = options)
+  res <- catalog_map(ctg, st_concave_hull, ...)
 
-  geom <- do.call(c, res)
-  sf::st_geometry(ctg@data) <- geom
+  sf::st_geometry(ctg@data) <- res
   attr(ctg, "trueshape") <- TRUE
   return(ctg)
 }
--- R/st_hull.R
+++ R/st_hull.R
@@ -17,6 +17,8 @@
 #'
 #' @param x,y An object of class LAS or XY coordinates of points in case of `concaveman`. This can be
 #' specified as two vectors x and y, a 2-column matrix x, a list with two components, etc.
+#' @param method string. currently support "concaveman"
+#' @param ... Propagate to the method
 #' @param concavity numeric a relative measure of concavity. 1 results in a relatively detailed shape,
 #' Infinity results in a convex hull. You can use values lower than 1, but they can produce pretty crazy
 #' shapes.
@@ -44,7 +46,7 @@ NULL
 
 #' @export
 #' @rdname st_hull
-st_concave_hull <- function(x, concavity = 2, length_threshold = 5) UseMethod("st_concave_hull")
+st_concave_hull <- function(x, method = "concaveman",  ...) UseMethod("st_concave_hull")
 
 
 #' @export
@@ -59,13 +61,27 @@ st_convex_hull.LAS = function(x)
 }
 
 #' @export
-st_concave_hull.LAS = function(x, concavity = 2, length_threshold = 5)
+st_concave_hull.LAS = function(x, method = "concaveman",  ...)
 {
-  chull <- concaveman(x$X, x$Y, concavity, length_threshold)
-  chull <- sf::st_polygon(list(as.matrix(chull)))
-  chull <- sf::st_geometry(chull)
-  sf::st_crs(chull) <- st_crs(x)
-  return(chull)
+  if (method == "concaveman")
+  {
+    chull <- concaveman(x$X, x$Y, ...)
+    chull <- sf::st_polygon(list(as.matrix(chull)))
+    chull <- sf::st_sfc(chull, crs = st_crs(x))
+    if (!sf::st_is_valid(chull)) chull <- sf::st_make_valid(chull)
+    return(chull)
+  }
+
+  if (method == "concavetin")
+  {
+    chull <- concavetin(x, ...)
+    chull <- sf::st_polygon(list(as.matrix(chull)))
+    chull <- sf::st_sfc(chull, crs = st_crs(x))
+    if (!sf::st_is_valid(chull)) chull <- sf::st_make_valid(chull)
+    return(chull)
+  }
+
+  stop("Method not supported. It should be one of 'concaveman' or 'concavetin'")
 }
 
 #' @rdname st_hull
@@ -74,13 +90,17 @@ concaveman <- function(x, y = NULL, concavity = 2, length_threshold = 0)
 {
   stopifnot(is.numeric(concavity), is.numeric(length_threshold), length(concavity) == 1L, length(length_threshold) == 1L)
 
-  if (is.null(y)) {
+  if (is.null(y))
+  {
     if (!is.matrix(x) && !is.data.frame(x) && !is.list(x)) stop("A matrix a list or a data.frame is expected.")
     if (dim(x)[2] != 2) stop("Two columns are expected.")
-    if (is.matrix(x)) {
+
+    if (is.matrix(x))
+    {
       y <- x[,2]
       x <- x[,1]
-    } else {
+    } else
+    {
       y <- x[[2]]
       x <- x[[1]]
     }
@@ -92,6 +112,146 @@ concaveman <- function(x, y = NULL, concavity = 2, length_threshold = 0)
   return(cpp_concaveman(x, y, concavity, length_threshold, h))
 }
 
+concavetin <- function(las, max_length = 8)
+{
+  assert_is_a_number(max_length)
+  assert_all_are_non_negative(max_length)
+
+  if (npoints(las) < 3) stop("Impossible to triangllate less than 3 points.", call. = FALSE)
+
+  # Triangulate the points and trim small triangles
+  D <- tDelaunay(las, trim = max_length)
+  X <- st_coordinates(las, z = FALSE)
+  #geometry::trimesh(D,X, asp = 1)
+
+  # Compute the contours of the triangulation
+  hull <- tin_exterior_ring(D, X)
+  return(hull)
+}
+
+tin_exterior_ring = function(D, X, s = 5)
+{
+  used <- N <- . <- P1 <- P2 <- NULL
+
+  # Get the contours of the partial triangulation
+  # *********************************************
+  D <- data.table::as.data.table(D)
+
+  # Order the Delaunay triangulation clockwise
+  D = t(apply(D, 1, function(x)
+  {
+    p1 <- c(X[x[1], 1], X[x[1], 2], 1)
+    p2 <- c(X[x[2], 1], X[x[2], 2], 1)
+    p3 <- c(X[x[3], 1], X[x[3], 2], 1)
+    M  <- rbind(p1, p2, p3)
+    clockwise <-  det(M) < 0
+    if (clockwise) return(x)
+    return(c(x[3], x[2], x[1]))
+  }))
+
+  # Compute the vertices (egde of each triangle)
+  n  <- nrow(D)
+  p1 <- integer(n*3)
+  p2 <- integer(n*3)
+  for (i in 1:n)
+  {
+    j <- 3*i-2
+    tri <- as.numeric(D[i,])
+    p1[j]     <- tri[1]
+    p2[j]     <- tri[2]
+    p1[1 + j] <- tri[1]
+    p2[1 + j] <- tri[3]
+    p1[2 + j] <- tri[2]
+    p2[2 + j] <- tri[3]
+  }
+
+  vertice  <- matrix(c(p1, p2), ncol = 2)
+  svertice <- t(apply(vertice, 1, sort))
+  svertice <- data.table::as.data.table(svertice)
+  vertice  <- data.table::as.data.table(vertice)
+  names(vertice) <- c("p1", "p2")
+
+  # Filter the contour vertices. Contour vertices are those
+  # that appear only once
+  notcontour1  <- duplicated(svertice)
+  notcontour2  <- duplicated(svertice, fromLast = TRUE)
+  contour      <- vertice[!(notcontour1|notcontour2)]
+  contour$used <- FALSE
+
+  # Extract the polygons from the contour
+  # *************************************
+  # Initialization
+  n  <- nrow(contour)
+  p1 <- numeric(n)
+  p2 <- numeric(n)
+
+  # The first vertice initiate a polygon
+  p1[1] <- contour$p1[1]
+  p2[1] <- contour$p2[1]
+  data.table::set(contour, 1L, 3L, TRUE)
+
+  # Find the last vertice that close the polygon
+  j <- which(contour$p1 == p1[1] & contour$used == FALSE)
+  s <- length(j) == 0L
+
+  if (s) j <- which(contour$p2 == p1[1] & contour$used == FALSE)
+  if (length(j) == 0L) stop("Internal error in tHull: please report this error (length(j) == 0L)")
+  if (length(j) > 1) j <- j[1]
+
+  if (s) {
+    p1[n] <- contour$p1[j]
+    p2[n] <- contour$p2[j]
+  } else {
+    p1[n] <- contour$p2[j]
+    p2[n] <- contour$p1[j]
+  }
+
+  data.table::set(contour, j, 3L, TRUE)
+
+  # Continue until the we reach the last vertice
+  for (i in 2:n)
+  {
+    j <- which(contour$p1 == p2[i - 1] & contour$used == FALSE)
+    s <- length(j) == 0L
+
+    if (s) j <- which(contour$p2 == p2[i - 1] & contour$used == FALSE)
+    if (length(j) == 0L) stop("Internal Error: please report this error (tHull L147)")
+    if (length(j) > 1) j <- j[1]
+
+    if (!s)
+    {
+      p1[i] <- contour$p1[j]
+      p2[i] <- contour$p2[j]
+    }
+    else
+    {
+      p1[i] <- contour$p2[j]
+      p2[i] <- contour$p1[j]
+    }
+
+    data.table::set(contour, j, 3L, TRUE)
+
+    if (p2[i] == p1[n]) {
+      contour <- contour[used == FALSE]
+      break
+    }
+  }
+
+  orderedcontour <- data.table::data.table(p1, p2)
+  orderedcontour <- orderedcontour[orderedcontour$p1 != 0,]
+
+  XX <- data.frame(P1 = X[orderedcontour$p1,1], P2 = X[orderedcontour$p1,2])
+  data.table::setDT(XX)
+
+  # Fix self intersection
+  XX[, N := .N , by = .(P1,P2)]
+  XX[c(1, .N), N := 1]
+  XX = XX[N == 1][, N := NULL][]
+
+  # Close polygon
+  XX <-rbind(XX, XX[1,])
+  return(XX)
+}
 
 convex_hull = function(x, y)
 {
