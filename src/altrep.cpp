b000a7068c36ede078d980e4cc4fe200362254a5
Dirk Eddelbuettel <edd@debian.org>
2025-12-26 08:39:52 -0600

    Accommodate R-release and earlier

--- src/altrep.cpp
+++ src/altrep.cpp
@@ -16,6 +16,7 @@ bool is_materialized(SEXP x)
 // [[Rcpp::export]]
 SEXP altrep_full_class(SEXP x) {
   if (!ALTREP(x)) return R_NilValue;
+#if R_VERSION >= R_Version(4, 6, 0)
   std::vector<std::string> v;
   auto visitor = [](SEXP name, SEXP attr, void* data) -> SEXP {
     std::vector<std::string>* ptr = static_cast<std::vector<std::string>*>(data);
@@ -25,4 +26,7 @@ SEXP altrep_full_class(SEXP x) {
   };
   R_mapAttrib(ALTREP_CLASS(x), visitor, static_cast<void*>(&v));
   return Rcpp::wrap(v);
+#else
+  return ATTRIB(ALTREP_CLASS(x));
+#fi
 }
