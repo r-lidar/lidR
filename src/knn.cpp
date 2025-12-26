422332b23fc6b914ca6c834fda14aadfca9decd7
Dirk Eddelbuettel <edd@debian.org>
2025-12-26 08:31:09 -0600

    Accomodate Rcpp change now returning R_xlen_t on nrow()

--- src/knn.cpp
+++ src/knn.cpp
@@ -93,7 +93,7 @@ List cpp_knnx(S4 data, S4 query, int k, int ncpu)
   NumericVector Z = tmp["Z"];
 
   int npoints = X.size();
-  k = std::min(k, temp.nrow());
+  k = std::min(static_cast<R_xlen_t>(k), temp.nrow());
 
   IntegerMatrix knn_idx(npoints, k);
   NumericMatrix knn_dist(npoints, k);
